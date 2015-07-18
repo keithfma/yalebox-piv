46ba769a4ed4a36d6adc64f2a0ac1b443dd9e5aa
keithma <keithma@bu.edu>
2015-07-18 16:40:13 -0400

    Completed get_color_ref.m

--- preprocess/get_color_ref.m
+++ preprocess/get_color_ref.m
@@ -1,4 +1,4 @@
-%function [color_ref, subset] = get_color_ref(image_file, subset)
+function [color_ref, subset] = get_color_ref(image_file, subset)
 % function [color_ref, subset] = get_color_ref(image_file, subset)
 %
 %
@@ -11,15 +11,53 @@
 %
 %   image_file = String. Filename of the image to be analyzed.
 %   subset = 4-element vector. Limits of the subset to be averaged, in the
-%           form [min_row, max_row, min_col, max_col]. Will be set
-%           interactively if not provided as an input argument.
+%           form [xmin ymin width height]. Will be set interactively if not 
+%           provided as an input argument.
 %   color_ref = 3 element vector. 24-bit RGB representation of the average
 %               color of sand.
 %
 % Keith Ma, July 2015
 
-% DEBUG: Run as a script with predefined inputs
-image_file = 'test_data/test_a_00.jpg';
-
 %% read in data, check for sane inputs
 
+assert(exist(image_file, 'file') == 2, 'image_file is not a valid filename');
+img = imread(image_file);
+assert(isa(img, 'uint8') && size(img,3) == 3, 'image is not 24-bit RGB');
+
+if nargin == 2
+    tmp = size(subset);
+    assert(numel(tmp) == 2 && min(tmp) == 1 && max(tmp) == 4, ...
+        'subset is not a 4-element vector');
+    assert(subset(1) >= 1 && subset(1)+subset(3) <= size(img,2) ...
+        && subset(2) >= 1 && subset(2)+subset(4) <= size(img,1), ...
+        'subset extends beyond the limits of the image data');
+end
+
+%% get subset extent (if not supplied)
+
+if nargin ==1
+    while 1
+        [img_subset, subset] = imcrop(img);
+        hfig = figure;
+        imshow(img_subset);
+        button = questdlg('Continue using this region?','get_color_ref','Continue','Retry','Cancel', 'Retry');
+        close(hfig);
+        switch button
+            case 'Continue'                
+                break
+            case 'Retry'
+                continue
+            case 'Cancel'
+                error('get_color_ref canceled by user');
+        end
+    end
+else
+    [img_subset, subset] = imcrop(img, subset);
+end
+
+%% compute average value
+
+color_ref = [0, 0, 0];
+for i = 1:3
+    color_ref(i) = mean(mean(double(img_subset(:,:,i))));
+end
