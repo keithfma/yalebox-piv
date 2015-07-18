148040d09268e70c5ead27b0d131f4309c4d62db
keithma <keithma@bu.edu>
2015-07-18 10:58:05 -0400

    create_mask: added checks for sane input arguments

--- preprocess/create_mask.m
+++ preprocess/create_mask.m
@@ -18,13 +18,29 @@
 %
 %   image_file = String. Filename of image to be analyzed.
 %   color_ref = 3 element vector.
-%   color_tol = Scalar.
-%   smooth_rad = Scalar.    
+%   color_tol = Scalar. Color tolerance as a normalized distance in ()
+%               colorspace, valid range is [0,1]
+%   smooth_rad = Scalar. Radius of smoothing filter in pixels.    
 %
 % Keith Ma, July 2015
 
 % DEBUG: Run as a script with predefined inputs
 image_file = 'test_data/test_a_00.jpg';
-color_ref = [];
-color_tol = [];
-smooth_rad = [];
+color_ref = [0, 0, 0];
+color_tol = 0.5;
+smooth_rad = 10;
+
+%% check for sane inputs
+
+assert(exist(image_file, 'file') == 2, 'image_file is not a valid filename');
+
+tmp = size(color_ref);
+assert(numel(tmp) == 2 && min(tmp) == 1 && max(tmp) ==3, ...
+    'color_ref is not a 3-element vector');
+
+assert(numel(color_tol) == 1, 'color_tol is not a scalar');
+assert(color_tol >= 0 && color_tol <= 1, 'color_tol is not in the range [0,1]');
+
+assert(numel(smooth_rad) == 1, 'smooth_rad is not a scalar');
+
+%% read in data
