6839cdbaccb3f0d735150532390b037e174aee34
keithma <keithma@bu.edu>
2015-07-18 22:46:33 -0400

    Tested colorspace appraoch, disappointing results.

--- preprocess/create_mask.m
+++ preprocess/create_mask.m
@@ -19,7 +19,7 @@
 %
 %   image_file = String. Filename of image to be analyzed.
 %   color_ref = 3 element vector. 24-bit RGB representation of the average
-%               color of sand.
+%               color of sand, valid range is 0-255.
 %   color_tol = Scalar. Color tolerance as distance in normalized RGB
 %               colorspace (where 0-255 is rescaled to 0-1 for each axis), valid range is [XXX]
 %   smooth_rad = Scalar. Radius of smoothing filter in pixels, can be fractional.    
@@ -27,33 +27,51 @@
 % Keith Ma, July 2015
 
 % DEBUG: Run as a script with predefined inputs
-image_file = 'test_data/test_a_00.jpg';
-color_ref = [0, 0, 0];
+image_file = 'test_data/test_a_00_crop.png';
+color_ref = [28.9640   28.6109   24.7218];
 color_tol = 0.5;
 smooth_rad = 10;
 
 %% read in data, check for sane inputs
 
 assert(exist(image_file, 'file') == 2, 'image_file is not a valid filename');
+img = imread(image_file);
+assert(isa(img, 'uint8') && size(img,3) == 3, 'image is not 24-bit RGB');
 
 tmp = size(color_ref);
-assert(numel(tmp) == 2 && min(tmp) == 1 && max(tmp) ==3, ...
+assert(numel(tmp) == 2 && min(tmp) == 1 && max(tmp) == 3, ...
     'color_ref is not a 3-element vector');
+assert(max(color_ref) <= 255, min(color_ref) >= 0, ...
+    'color_ref is not in the range [0. 255]');
 
 assert(numel(color_tol) == 1, 'color_tol is not a scalar');
 assert(color_tol >= 0 && color_tol <= 1, 'color_tol is not in the range [0,1]');
 
 assert(numel(smooth_rad) == 1, 'smooth_rad is not a scalar');
 
-img = imread(image_file);
-assert(isa(img, 'uint8') && size(img,3) == 3, 'image is not 24-bit RGB');
 
 %% smooth image
 
-filt = fspecial('disk', smooth_rad);
-img_smooth = imfilter(img, filt);
+%filt = fspecial('disk', smooth_rad);
+%img = imfilter(img, filt);
 
-%% compute normalized distances...
+N = 20;
+img = double(img)/255;
+for i = 1:3
+    img(:,:,i) = medfilt2(img(:,:,i));
+end
+
+% %% normalize colors
+% 
+% img = double(img)/255;
+% color_ref = color_ref/255;
+% 
+% %% compute distances to reference color
+% 
+% img_dist = sqrt( (img(:,:,1)-color_ref(1)).^2 + (img(:,:,2)-color_ref(2)).^2 + (img(:,:,3)-color_ref(3)).^2 );
+
+
+
 
 
 
