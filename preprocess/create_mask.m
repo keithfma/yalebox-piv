2509951736ef41ac6bddce325238747a1d0fe740
keithma <keithma@bu.edu>
2015-07-19 11:13:20 -0400

    Changed create_mask to accept image matrix, rather than filename
    This input format will make it easier to integrate several processing steps in
    to one script.

--- preprocess/create_mask.m
+++ preprocess/create_mask.m
@@ -1,5 +1,5 @@
-function [mask] = create_mask(image_file, entropy_radius, open_radius, bw_threshold)
-% function [mask] = create_mask(image_file, entropy_radius, open_radius, bw_threshold)
+function [mask] = create_mask(img, entropy_radius, open_radius, bw_threshold)
+% function [mask] = create_mask(img, entropy_radius, open_radius, bw_threshold)
 %
 % Create a logical mask for a color image (image_file) that is TRUE where
 % there is sand and FALSE elsewhere. This can be used to remove (set to 0)
@@ -15,24 +15,25 @@ function [mask] = create_mask(image_file, entropy_radius, open_radius, bw_thresh
 %   (4) Convert to a T/F mask by simple thresholding
 %
 % Arguments:
-%   image_file = String
-%   entropy_radius = Scalar
-%   open_radius = Scalar
-%   bw_threshold = Scalar in the range [0,1]
+%   img = MxNx3 matrix of type uint8, a 24-bit (Truecolor) RGB image 
+%   entropy_radius = Scalar. Radius of neighboorhood included in the
+%       entropy filter calculation for each pixel, [pixels]
+%   open_radius = Scalar. Radius of the structuring element used for the
+%       morphological opening filter, [pixels]
+%   bw_threshold = Scalar in the range [0,1], threshold value used to
+%       convert mask image from grayscale to black-and-white.
 %
 % Keith Ma, July 2015
 
 % % DEBUG: Run as a script with predefined inputs
-% image_file = 'test_data/test_a_00_crop.png';
+% img = imread('test_data/test_a_00_crop.png');
 % entropy_radius = 9; % pixels
 % open_radius = 36; % pixels
 % bw_threshold = 0.75; % normalized
 
 %% read in data, check for sane inputs
 
-assert(exist(image_file, 'file') == 2, 'image_file is not a valid filename');
-mask = imread(image_file);
-assert(isa(mask, 'uint8') && size(mask,3) == 3, 'image is not 24-bit RGB');
+assert(isa(img, 'uint8') && size(img,3) == 3, 'image is not 24-bit RGB');
 
 assert(numel(entropy_radius) == 1, 'entropy_radius is not a scalar');
 assert(entropy_radius >= 0, 'entropy_radius is not positive');
@@ -47,15 +48,15 @@ assert(bw_threshold >= 0 && bw_threshold <= 1, ...
 
 %% compute mask
 
-mask = rgb2gray(mask);
+img = rgb2gray(img);
 
 nhood = getnhood(strel('disk', entropy_radius)); 
-mask = entropyfilt(mask, nhood);
+img = entropyfilt(img, nhood);
 
 s = strel('disk', open_radius);
-mask = imopen(mask, s);
+img = imopen(img, s);
 
-mask = mask-min(mask(:));
-mask = mask./max(mask(:));
-mask = im2bw(mask, bw_threshold);
+img = img-min(img(:));
+img = img./max(img(:));
+mask = im2bw(img, bw_threshold);
 
