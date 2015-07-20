febcc7f3c3cf82fb33eef59858a209bca4bd986d
keithma <keithma@bu.edu>
2015-07-20 18:41:50 -0400

    Updated comments and deleted obsolete code.

--- preprocess/stretch_color.m
+++ preprocess/stretch_color.m
@@ -1,30 +1,27 @@
-function [img_stretch] = stretch_color(img, mask)
-% Stretch colors to make full use of the available range. Threshold values
-% are computed as a function of x using a sliding window quantile filter.
-
-% Arguments
-lower_limit = 0.01; % quantile
-upper_limit = 0.99; % quantile
-%width = 100; % pixels
+function [img_stretch, stretch_min, stretch_rng] = stretch_color(img, mask, upper_limit, lower_limit)
+%
+% Compute and/or apply stretch correction to make colors use the full
+% available range. Stretch correction vectors as a function of x are
+% computed from a LOESS curve fit to per-column quantiles. The stretched
+% image is (img-stretch_min)/stretch_rng, truncated to the range [0, 1].
+% The stretch is computed independently for each color band.
+%
+%
+% Usage:
+%   Compute and apply:
+%   Compute only:
+%   Correct only:
+%
+% Arguments:
+%   img = 
+%   mask = 
+%   upper_limit = 
+%   lower_limit = 
+%
+% Keith Ma, July 2015
 
 % DEBUG: grayscale
 img = rgb2gray(img);
-
-%% sliding window quantiles 
- 
-% ncol = size(img,2);
-% x = 1:ncol;
-% lower = nan(size(x));
-% upper = nan(size(x));
-% for i = x
-%     start = floor(max(1, i-width/2));
-%     stop = ceil(min(ncol, i+width/2));
-%     img_sub = img(:,start:stop);
-%     mask_sub = mask(:,start:stop);
-%     tmp = quantile(img_sub(mask_sub), [lower_limit, upper_limit]);
-%     lower(i) = tmp(1);
-%     upper(i) = tmp(2);
-% end
 
 %% get correction from smoothed per-column quantiles
 
