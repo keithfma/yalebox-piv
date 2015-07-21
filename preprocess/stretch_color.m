ab8e5d94fb6ccb28948fe77229d8ea5a61df8f1b
keithma <keithma@bu.edu>
2015-07-20 23:33:19 -0400

    Working, uses windowed quantiles, needs documentation and checks

--- preprocess/stretch_color.m
+++ preprocess/stretch_color.m
@@ -1,11 +1,12 @@
-function [img_stretch, stretch_min, stretch_rng] = stretch_color(img, mask, upper_limit, lower_limit)
+function [img_stretch, stretch_min, stretch_rng] = stretch_color(img, mask, trim, nbin, loess_frac)
+% [img_stretch, stretch_min, stretch_rng] = ...
+%       stretch_color(img, mask, trim, nbin, loess_frac)
 %
 % Compute and/or apply stretch correction to make colors use the full
 % available range. Stretch correction vectors as a function of x are
-% computed from a LOESS curve fit to per-column quantiles. The stretched
+% computed from a LOESS curve fit to windowed quantiles. The stretched
 % image is (img-stretch_min)/stretch_rng, truncated to the range [0, 1].
 % The stretch is computed independently for each color band.
-%
 %
 % Usage:
 %   Compute and apply:
@@ -15,46 +16,53 @@ function [img_stretch, stretch_min, stretch_rng] = stretch_color(img, mask, uppe
 % Arguments:
 %   img = 
 %   mask = 
-%   upper_limit = 
-%   lower_limit = 
-%
+%   trim =
+%   nbin = 
+%   loess_frac = 
+%   
 % Keith Ma, July 2015
 
-% DEBUG: grayscale
-img = rgb2gray(img);
 
-%% get correction from smoothed per-column quantiles
+%% check for sane arguments
 
-ncol = size(img,2);
-x = 1:ncol;
-lower = nan(size(x));
-upper = nan(size(x));
-for i = x
-    img_sub = img(:,i);
-    mask_sub = mask(:,i);
-    tmp = quantile(img_sub(mask_sub), [lower_limit, upper_limit]);
-    lower(i) = tmp(1);
-    upper(i) = tmp(2);
+get_stretch = 1;
+apply_stretch = 1;
+img_gray = rgb2gray(img);
+
+%% get correction from windowed quantiles
+
+if get_stretch
+    
+    edge = round(linspace(1, size(img,2), nbin));
+    mid = edge(1:end-1)+diff(edge)/2;
+    lower = nan(1, nbin-1);
+    upper = nan(1, nbin-1);
+    
+    for i = 1:nbin-1
+        img_sub = img_gray(:,edge(i):edge(i+1));
+        mask_sub = mask(:,edge(i):edge(i+1));
+        tmp = quantile(img_sub(mask_sub), trim);
+        lower(i) = tmp(1);
+        upper(i) = tmp(2);
+    end
+    
+    lower_fit = loess(mid, lower, mid, loess_frac, 1, 0);
+    upper_fit = loess(mid, upper, mid, loess_frac, 1, 0);
+    
+    lower_smooth = interp1(mid, lower_fit, 1:size(img,2), 'linear');
+    upper_smooth = interp1(mid, upper_fit, 1:size(img,2), 'linear');
+    
+    stretch_min = lower_smooth;
+    stretch_rng = upper_smooth-lower_smooth;
+    
 end
-
-x_fit = downsample(x,10);
-if x_fit(end) ~= x(end); 
-    x_fit(end+1) = x(end);
-end
-
-lower_fit = loess(x, lower, x_fit, 0.25, 1, 0);
-upper_fit = loess(x, upper, x_fit, 0.25, 1, 0);
-
-lower_smooth = interp1(x_fit, lower_fit, x, 'linear');
-upper_smooth = interp1(x_fit, upper_fit, x, 'linear');
-
-stretch_min = lower_smooth;
-stretch_rng = upper_smooth-lower_smooth;
 
 %% apply stretch
 
-img_stretch = img-repmat(stretch_min, size(img,1), 1);
-img_stretch = img_stretch./repmat(stretch_rng, size(img,1), 1);
-img_stretch(:) = min(1, max(0, img_stretch(:)));
-
-
+if apply_stretch
+    
+    img_stretch = img-repmat(stretch_min, size(img,1), 1, 3);
+    img_stretch = img_stretch./repmat(stretch_rng, size(img,1), 1, 3);
+    img_stretch(:) = min(1, max(0, img_stretch(:)));
+    
+end
