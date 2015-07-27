edbe235e0565c42ba2048d60afa560417b910058
keithma <keithma@bu.edu>
2015-07-26 21:56:36 -0400

    Deleted the obsolete masking routine.

--- preprocess/correct_mask.m
+++ preprocess/correct_mask.m
@@ -1,60 +1,119 @@
-function [mask] = correct_mask(img, entropy_radius, open_radius, bw_threshold)
-% function [mask] = correct_mask(img, entropy_radius, open_radius, bw_threshold)
+function [mask] = correct_mask_b(rgb, hue_lim, value_lim, entropy_lim, ...
+                               median_window, entropy_window, opening_radius)
 %
-% Create a logical mask for a color image (image_file) that is TRUE where
-% there is sand and FALSE elsewhere. This can be used to remove (set to 0)
-% the background in a image prior to PIV analysis or other applications.
+% Create a logical mask for a color image that is TRUE where there is sand and
+% FALSE elsewhere. This can be used to remove (set to 0) the background in a
+% image prior to PIV analysis or other applications. Sand is identified by
+% remapping to HSV colorspace, filtering and thresholding the "hue" and "value"
+% bands.
 %
-% Sand is identified by filtering and thresholding the image data. The
-% steps are:
-% 
-%   (1) Convert color to grayscale
-%   (2) Apply entropy filter, which provides a measure of local texture
-%   (3) Apply opening filter, which eliminates narrow curvilinear objects
-%       typical of reflections
-%   (4) Convert to a T/F mask by simple thresholding
+% All arguments MUST be provided. Default values will be used for any arguments
+% set to the empty matrix []. The default values were effective for test data
+% used in development, but are unlikely to work for all images. 
 %
 % Arguments:
-%   img = Double, 3D matrix. RGB image, normalized to the range [0, 1]
-%   entropy_radius = Scalar. Radius of neighboorhood included in the
-%       entropy filter calculation for each pixel, [pixels]
-%   open_radius = Scalar. Radius of the structuring element used for the
-%       morphological opening filter, [pixels]
-%   bw_threshold = Scalar in the range [0,1], threshold value used to
-%       convert mask image from grayscale to black-and-white.
-%   mask = Logical, 2D matrix. TRUE where there is sand, FALSE elsewhere
+%
+%   im = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
+%       MATLAB with imread()
+%
+%   hue_lim = 2-element vector, double, range [0, 1]. [minimum, maximum] HSV
+%     "hue" included as sand in the mask. Default = [0.0, 0.5]
+%
+%   value_lim = 2-element vector, double, range [0,1]. [minimum, maximum] HSV
+%     "value" included as sand in the mask, . Default = [0.0, 0.5]
+%
+%   entropy_lim = 2-element vector, double, range [0, 1]. [minimum, maximum]
+%     entropy included as sand in the mask. Default = [0.5, 1.0]
+%
+%   median_window = scalar, integer, window size in pixels for median filter
+%     applied to all bands. Default = 25.
+%
+%   entropy_window = scalar, integer, window size in pixels for entropy filter.
+%     Default = 9.
+%
+%   opening_radius = scalar, double, radius of disk structuring element used in
+%     mophological opening filter. Default = 10.
 %
 % Keith Ma, July 2015
 
-%% read in data, check for sane inputs
+%% DEBUG
+%median_size = 25;
+%entropy_size = 9;
+%hue_range = [0, 0.4]; % min, max
+%value_range = [0, 0.5];
+%entropy_range = [0.5, 1]; 
+%morph_radius = 10;
+%% END DEBUG
 
-assert(isa(img, 'double'), 'img is not of type double');
-assert(size(img,3) == 3, 'img is not a 3D array (RGB)');
-assert(max(img(:)) <= 1 && min(img(:)) >= 0, 'img is not in the range [0,1]');
+% set default values
+if isempty(hue_lim); hue_lim = [0.0, 0.5]; end
+if isempty(value_lim); value_lim = [0.0, 0.5]; end
+if isempty(entropy_lim); entropy_lim = [0.5, 1.0]; end
+if isempty(median_window); median_window = 25; end
+if isempty(entropy_window); entropy_window = 9; end
+if isempty(opening_radius); opening_radius = 10; end
 
-assert(numel(entropy_radius) == 1, 'entropy_radius is not a scalar');
-assert(entropy_radius >= 0, 'entropy_radius is not positive');
+% check for sane arguments, set default values
+assert(isa(rgb, 'uint8'), 'img_rgb is not type "uint8"');
+assert(size(rgb,3) == 3, 'img_rgb is not 3-dimensional');
+assert(numel(hue_lim)==2 && min(size(hue_lim)) == 1, ...
+    'hue_lim is not a 2-element vector');
+assert(min(hue_lim) >= 0 && max(hue_lim) <= 1, ...
+    'hue_lim is not in the range [0,1]');
+assert(numel(value_lim)==2 && min(size(value_lim)) == 1, ...
+    'value_lim is not a 2-element vector');
+assert(min(value_lim) >= 0 && max(value_lim) <= 1, ...
+    'value_lim is not in the range [0,1]');
+assert(numel(entropy_lim)==2 && min(size(entropy_lim)) == 1, ...
+    'entropy_lim is not a 2-element vector');
+assert(min(entropy_lim) >= 0 && max(entropy_lim) <= 1, ...
+    'entropy_lim is not in the range [0,1]');
+assert(numel(median_window) == 1 && median_window > 0 && ...
+    round(median_window) == median_window, ...
+    'median_window is not a positive integer');
+assert(numel(entropy_window) == 1 && entropy_window > 0 && ...
+    round(entropy_window) == entropy_window, ...
+    'entropy_window is not a positive integer');
+assert(numel(opening_radius) == 1 && opening_radius > 0, ...
+    'opening_radius is not a positive scalar');
 
-assert(numel(open_radius) == 1, 'open_radius is not a scalar');
-assert(open_radius >= 0, 'open_radius is not positive');
+% get hue, value, and entropy, normalized to the range [0, 1]
+hsv = rgb2hsv(rgb); 
+hue = hsv(:,:,1);
+value = hsv(:,:,3);
+entropy = entropyfilt(value, true(entropy_window));
 
-assert(numel(bw_threshold) == 1, 'bw_threshold is not a scalar');
-assert(bw_threshold >= 0 && bw_threshold <= 1, ...
-    'bw_threshold is not in the range [0, 1]');
+% smooth, preseving edges with median filter
+hue = medfilt2(hue, median_window*[1, 1]);
+value = medfilt2(value, median_window*[1, 1]);
+entropy = medfilt2(entropy, median_window*[1, 1]);
 
+% normalize range to [0,1]
+hue = hue-min(hue(:)); hue = hue./max(hue(:));
+value = value-min(value(:)); value = value./max(value(:));
+entropy = entropy-min(entropy(:)); entropy = entropy./max(entropy(:));
 
-%% compute mask
+% threshold bands
+hue_mask = hue >= hue_lim(1) & hue <= hue_lim(2);
+value_mask = value >= value_lim(1) & value <= value_lim(2);
+entropy_mask = entropy >= entropy_lim(1) & entropy <= entropy_lim(2);
 
-img = rgb2gray(img);
+% create mask
+mask = hue_mask & value_mask & entropy_mask;
 
-nhood = getnhood(strel('disk', entropy_radius)); 
-entr = entropyfilt(img, nhood);
+% fill holes - top and bottom
+wall = true(1, size(mask,2));
+mask = [wall; mask; wall];
+mask = imfill(mask, 'holes');
+mask = mask(2:end-1, :);
 
-keyboard
+% fill holes - left and right
+wall = true(size(mask, 1), 1);
+mask = [wall, mask, wall];
+mask = imfill(mask, 'holes');
+mask = mask(:, 2:end-1);
 
-s = strel('disk', open_radius);
-img = imopen(img, s);
-
-img = img-min(img(:));
-img = img./max(img(:));
-mask = im2bw(img, bw_threshold);
+% clean up edges with morphological opening filter 
+% NOTE: CLOSE TOO? ORDER?
+disk = strel('disk', opening_radius);
+mask = imopen(mask, disk);
