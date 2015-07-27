070ec96bded901381e2480054c5db34a578bdc6b
keithma <keithma@bu.edu>
2015-07-26 21:44:10 -0400

    Added defaults and assertions for arguments, changed names.

--- preprocess/correct_mask_b.m
+++ preprocess/correct_mask_b.m
@@ -1,52 +1,81 @@
-%function [mask] = correct_mask(image_rgb, hue_range, value_range, entropy_range,
+%function [mask] = correct_mask(image_rgb, hue_lim, value_lim, entropy_lim,
 %                               median_window, entropy_window, opening_radius)
 %
 % Create a logical mask for a color image that is TRUE where there is sand and
 % FALSE elsewhere. This can be used to remove (set to 0) the background in a
-% image prior to PIV analysis or other applications.
+% image prior to PIV analysis or other applications. Sand is identified by
+% remapping to HSV colorspace, filtering and thresholding the "hue" and "value"
+% bands.
 %
-% Sand is identified by remapping to HSV colorspace, filtering and thresholding
-% the "hue" and "value" bands, . Default values for the various parameters were
-% effective for the test data used in development, but may well be inadequate
-% for other images.
+% All arguments MUST be provided. Default values will be used for any arguments
+% set to the empty matrix []. The default values were effective for test data
+% used in development, but are unlikely to work for all images. 
 %
 % Arguments:
 %
 %   im_rgb = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
 %       MATLAB with imread()
 %
-%   hue_range = 2-element vector, double, range [0, 1]. [minimum, maximum] HSV
+%   hue_lim = 2-element vector, double, range [0, 1]. [minimum, maximum] HSV
 %     "hue" included as sand in the mask. Default = [0.0, 0.5]
 %
-%   value_range = 2-element vector, double, range [0,1]. [minimum, maximum] HSV
+%   value_lim = 2-element vector, double, range [0,1]. [minimum, maximum] HSV
 %     "value" included as sand in the mask, . Default = [0.0, 0.5]
 %
-%   entropy_range = 2-element vector, double, range [0, 1]. [minimum, maximum]
+%   entropy_lim = 2-element vector, double, range [0, 1]. [minimum, maximum]
 %     entropy included as sand in the mask. Default = [0.5, 1.0]
 %
 %   median_window = scalar, integer, window size in pixels for median filter
-%     applied to all bands
+%     applied to all bands. Default = 25.
 %
-%   entropy_window = scalar, integer, window size in pixels for entropy filter
+%   entropy_window = scalar, integer, window size in pixels for entropy filter.
+%     Default = 9.
 %
 %   opening_radius = scalar, double, radius of disk structuring element used in
-%     mophological opening filter
+%     mophological opening filter. Default = 10.
 %
 % Keith Ma, July 2015
 
-% DEBUG
-median_size = 25;
-entropy_size = 9;
-hue_range = [0, 0.4]; % min, max
-value_range = [0, 0.5];
-entropy_range = [0.5, 1]; 
-morph_radius = 10;
-% END DEBUG
+%% DEBUG
+%median_size = 25;
+%entropy_size = 9;
+%hue_range = [0, 0.4]; % min, max
+%value_range = [0, 0.5];
+%entropy_range = [0.5, 1]; 
+%morph_radius = 10;
+%% END DEBUG
+
+% set default values
+if isempty(hue_lim); hue_lim = [0.0, 0.5]; end
+if isempty(value_lim); value_lim = [0.0, 0.5]; end
+if isempty(entropy_lim); entropy_lim = [0.5, 1.0]; end
+if isempty(median_window); median_window = 25; end
+if isempty(entropy_window); entropy_window = 9; end
+if isempty(opening_radius); opening_radius = 10; end
 
 % check for sane arguments, set default values
-
 assert(isa(img_rgb, 'uint8'), 'img_rgb is not type "uint8"');
 assert(size(img_rgb,3) == 3, 'img_rgb is not 3-dimensional');
+assert(numel(hue_lim)==2 && min(size(hue_lim)) == 1, ...
+    'hue_lim is not a 2-element vector');
+assert(min(hue_lim) >= 0 && max(hue_lim) <= 1, ...
+    'hue_lim is not in the range [0,1]');
+assert(numel(value_lim)==2 && min(size(value_lim)) == 1, ...
+    'value_lim is not a 2-element vector');
+assert(min(value_lim) >= 0 && max(value_lim) <= 1, ...
+    'value_lim is not in the range [0,1]');
+assert(numel(entropy_lim)==2 && min(size(entropy_lim)) == 1, ...
+    'entropy_lim is not a 2-element vector');
+assert(min(entropy_lim) >= 0 && max(entropy_lim) <= 1, ...
+    'entropy_lim is not in the range [0,1]');
+assert(numel(median_window) == 1 && median_window > 0 && ...
+    round(median_window) == median_window, ...
+    'median_window is not a positive integer');
+assert(numel(entropy_window) == 1 && entropy_window > 0 && ...
+    round(entropy_window) == entropy_window, ...
+    'entropy_window is not a positive integer');
+assert(numel(opening_radius) == 1 && opening_radius > 0, ...
+    'opening_radius is not a positive scalar');
 
 % get hue, value, and entropy, normalized to the range [0, 1]
 img_hsv = rgb2hsv(img_rgb); 
