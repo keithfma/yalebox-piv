2ee3b428d1828de24edd35e7e1527a8270ba7932
keithma <keithma@bu.edu>
2015-07-26 21:48:57 -0400

    Clean up and renamed variables throughout correct_mask_b

--- preprocess/correct_mask_b.m
+++ preprocess/correct_mask_b.m
@@ -1,4 +1,4 @@
-%function [mask] = correct_mask(image_rgb, hue_lim, value_lim, entropy_lim,
+%function [mask] = correct_mask(rgb, hue_lim, value_lim, entropy_lim,
 %                               median_window, entropy_window, opening_radius)
 %
 % Create a logical mask for a color image that is TRUE where there is sand and
@@ -13,7 +13,7 @@
 %
 % Arguments:
 %
-%   im_rgb = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
+%   im = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
 %       MATLAB with imread()
 %
 %   hue_lim = 2-element vector, double, range [0, 1]. [minimum, maximum] HSV
@@ -54,8 +54,8 @@ if isempty(entropy_window); entropy_window = 9; end
 if isempty(opening_radius); opening_radius = 10; end
 
 % check for sane arguments, set default values
-assert(isa(img_rgb, 'uint8'), 'img_rgb is not type "uint8"');
-assert(size(img_rgb,3) == 3, 'img_rgb is not 3-dimensional');
+assert(isa(rgb, 'uint8'), 'img_rgb is not type "uint8"');
+assert(size(rgb,3) == 3, 'img_rgb is not 3-dimensional');
 assert(numel(hue_lim)==2 && min(size(hue_lim)) == 1, ...
     'hue_lim is not a 2-element vector');
 assert(min(hue_lim) >= 0 && max(hue_lim) <= 1, ...
@@ -78,41 +78,41 @@ assert(numel(opening_radius) == 1 && opening_radius > 0, ...
     'opening_radius is not a positive scalar');
 
 % get hue, value, and entropy, normalized to the range [0, 1]
-img_hsv = rgb2hsv(img_rgb); 
-hue = img_hsv(:,:,1);
-value = img_hsv(:,:,3);
-entropy = entropyfilt(value, true(entropy_size));
+hsv = rgb2hsv(rgb); 
+hue = hsv(:,:,1);
+value = hsv(:,:,3);
+entropy = entropyfilt(value, true(entropy_window));
 
 % smooth, preseving edges with median filter
-hue_m = medfilt2(hue, median_size*[1, 1]);
-value_m = medfilt2(value, median_size*[1, 1]);
-entropy_m = medfilt2(entropy, median_size*[1, 1]);
+hue = medfilt2(hue, median_window*[1, 1]);
+value = medfilt2(value, median_window*[1, 1]);
+entropy = medfilt2(entropy, median_window*[1, 1]);
 
 % normalize range to [0,1]
-hue_m = hue_m-min(hue_m(:)); hue_m = hue_m./max(hue_m(:));
-value_m = value_m-min(value_m(:)); value_m = value_m./max(value_m(:));
-entropy_m = entropy_m-min(entropy_m(:)); entropy_m = entropy_m./max(entropy_m(:));
+hue = hue-min(hue(:)); hue = hue./max(hue(:));
+value = value-min(value(:)); value = value./max(value(:));
+entropy = entropy-min(entropy(:)); entropy = entropy./max(entropy(:));
 
 % threshold bands
-hue_t = hue_m >= hue_range(1) & hue_m <= hue_range(2);
-value_t = value_m >= value_range(1) & value_m <= value_range(2);
-entropy_t = entropy_m >= entropy_range(1) & entropy_m <= entropy_range(2);
+hue_mask = hue >= hue_lim(1) & hue <= hue_lim(2);
+value_mask = value >= value_lim(1) & value <= value_lim(2);
+entropy_mask = entropy >= entropy_lim(1) & entropy <= entropy_lim(2);
 
 % create mask
-mask = hue_t & value_t & entropy_t;
+mask = hue_mask & value_mask & entropy_mask;
 
 % fill holes - top and bottom
 wall = true(1, size(mask,2));
-mask_c = [wall; mask; wall];
-mask_c = imfill(mask_c, 'holes');
-mask_c = mask_c(2:end-1, :);
+mask = [wall; mask; wall];
+mask = imfill(mask, 'holes');
+mask = mask(2:end-1, :);
 
 % fill holes - left and right
 wall = true(size(mask, 1), 1);
-mask_c = [wall, mask_c, wall];
-mask_c = imfill(mask_c, 'holes');
-mask_c = mask_c(:, 2:end-1);
+mask = [wall, mask, wall];
+mask = imfill(mask, 'holes');
+mask = mask(:, 2:end-1);
 
 % clean up edges with morphological opening filter 
 disk = strel('disk', morph_radius);
-mask_c = imopen(mask_c, disk);
+mask = imopen(mask, disk);
