0593f68bb264a3e7a2a76bdeba36b40c9a48fa21
keithma <keithma@bu.edu>
2015-07-28 17:12:45 -0500

    correct_color: uses npts to select number of bins, corrections are matrices

--- preprocess/correct_color.m
+++ preprocess/correct_color.m
@@ -1,5 +1,5 @@
-function [baseline, scale] = correct_color(img, mask, width, step, quant, lfrac)
-% function [baseline, scale] = correct_color(img, mask, width, step, quant, lfrac)
+function [baseline, scale] = correct_color(rgb, mask, width, npts, quant, lfrac)
+% function [baseline, scale] = correct_color(rgb, mask, width, npts, quant, lfrac)
 %
 % Compute color baseline and scale corrections for masked sandbox image.
 % Specifically, sandbox images frequently have lateral gradients (with
@@ -17,15 +17,15 @@ function [baseline, scale] = correct_color(img, mask, width, step, quant, lfrac)
 %
 % Arguments:
 %
-%   img = 3D matrix, double, RGB image, normalized to the range [0, 1]
+%   rgb = 3D matrix, double, TRUECOLOR RGB 
 %
 %   mask = 2D matrix, double, TRUE where there is sand, FALSE elsewhere
 %
 %   width = Scalar, double, width of the sliding window for quantile
 %       calculations, in (whole) pixels
 %
-%   step = Scalar, double, spacing of windows for quantile calculations, in
-%       (whole) pixels
+%   npts = Scalar, integer, number of linearly spaced points at which
+%       quantiles are computed.
 %
 %   quant = 2-element vector, double, [lower, upper] quantiles to be
 %       calculated (and truncated by the correction), in the range 0 to 1
@@ -41,67 +41,55 @@ function [baseline, scale] = correct_color(img, mask, width, step, quant, lfrac)
 
 % set defaults
 if isempty(width); width = 200; end 
-if isempty(step); step = 50; end
+if isempty(npts); npts = 100; end
 if isempty(quant); quant = [0.01, 0.99]; end
 if isempty(lfrac); lfrac = 0.2; end
 
 % check for sane inputs
-assert(isa(img, 'double'), ...
-    'img is not of type double');
-assert(size(img,3) == 3, ...
-    'img is not a 3D array (RGB)');
+assert(isa(rgb, 'uint8') && size(rgb,3) == 3, ...
+     'rgb is not a Truecolor (24-bit) RGB image');
 assert(isa(mask, 'logical'), ...
-    'mask is not of type logical');
-assert(size(mask,1) == size(img, 1) && size(mask,2) == size(img, 2), ...
-    'mask and image dimensions do not match');
-assert(numel(width) == 1, ...
-    'width is not a scalar');
-assert(mod(width,1) == 0, ...
-    'width is not an integer');
-assert(numel(step) == 1, ...
-    'step is not a scalar');
-assert(mod(step,1) == 0, ...
-    'step is not an integer');
-assert(numel(quant) == 2, ...
-    'quant is not a 2-element vector');
-assert(max(quant) >=  0 && min(quant) <= 1, ...
-    'quant is not in the range 0-1');
-assert(numel(lfrac) == 1, ...
-    'lfrac is not a scalar');
-assert(max(lfrac) >=  0 && min(lfrac) <= 1, ...
-    'lfrac is not in the range 0-1');
+    'mask is not of type logical ');
+assert(size(mask,1) == size(rgb, 1) && size(mask,2) == size(rgb, 2), ...
+    'mask and rgb dimensions do not match');
+assert(numel(width) == 1 && mod(width,1) == 0, ...
+    'width is not a scalar integer');
+assert(numel(npts) == 1 && mod(npts,1) == 0, ...
+    'npts is not a scalar integer');
+assert(numel(quant) == 2 && max(quant) >=  0 && min(quant) <= 1, ...
+    'quant is not a 2-element vector in the range 0-1');
+assert(numel(lfrac) == 1 && max(lfrac) >=  0 && min(lfrac) <= 1, ...
+    'lfrac is not a scalar in the range 0-1');
 
-%% get corrections from windowed quantiles
+% convert image to 2D intensity matrix
+hsv = rgb2hsv(rgb);
+val = hsv(:,:,3);
+[nrow, ncol] = size(val);
 
-% convert image to grayscale
-imgg = rgb2gray(img);
+% create bins, truncated at image edges
 
-% get windows
-ncol = size(img,2);
-x = 1:step:ncol;
-x0 = max(1, x-ceil(width/2));
-x1 = min(ncol, x+ceil(width/2));
+x = linspace(1, ncol, npts);
+x0 = max(1, round(x-width));
+x1 = min(ncol, round(x+width));
 
-% compute quantiles
-nx = numel(x);
-top = nan(1, nx);
-bot = nan(1, nx);
-for i = 1:nx
-    imgg_win = imgg(:,x0(i):x1(i));
+% windowed quantiles for intensity
+top = nan(1, npts);
+bot = nan(1, npts);
+for i = 1:npts
+    val_win = val(:,x0(i):x1(i));
     mask_win = mask(:,x0(i):x1(i));
-    tmp = quantile(imgg_win(mask_win), quant);
+    tmp = quantile(val_win(mask_win), quant);
     bot(i) = tmp(1);
     top(i) = tmp(2);
 end
 
-% smooth quantiles
+% smooth and upscale quantiles
 top = loess(x, top, x, lfrac, 1, 0);
-bot = loess(x, bot, x, lfrac, 1, 0);
-
-% upscale smooth quantiles
 top = interp1(x, top, 1:ncol, 'linear');
+
+bot = loess(x, bot, x, lfrac, 1, 0);
 bot = interp1(x, bot, 1:ncol, 'linear');
 
-% corrections
-baseline = bot;
-scale = 1./(top-bot);
+% correction matrices from smooth quantiles
+baseline = repmat(bot,          nrow, 1);
+scale =    repmat(1./(top-bot), nrow, 1);
