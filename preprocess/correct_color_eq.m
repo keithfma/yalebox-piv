0b2de110aef9c777e4278d3f78f8e4076623fea6
keithma <keithma@bu.edu>
2015-07-29 17:00:45 -0500

    color correction via histogram equalization working well

--- preprocess/correct_color_eq.m
+++ preprocess/correct_color_eq.m
@@ -1,10 +1,10 @@
-function [valeq] = correct_color_eq(rgb, mask, width, nbin)
+function [valeq] = correct_color_eq(rgb, mask, width)
 
 % convert image to 2D intensity matrix
 hsv = rgb2hsv(rgb);
 val = hsv(:,:,3);
 [nrow, ncol] = size(val);
- 
+
 % equalize histogram, column-by-column
 valeq = zeros([nrow, ncol]);
 for i = 1:ncol
@@ -14,14 +14,18 @@ for i = 1:ncol
     ind(ind<=0) = abs(ind(ind<=0))+2;
     ind(ind>ncol) = 2*ncol-ind(ind>ncol);
     
-    % extract sand pixels in window and equalize
-    val_win = val(:, ind);
-    mask_win = mask(:, ind);
-    eql = histeq(val_win(mask_win), nbin);
-    val_win(mask_win) = eql;
+    % mask for sand pixels in col and in window
+    mask_col = false(size(mask));
+    mask_col(:,i) = mask(:,i);
+    mask_win = false(size(mask));
+    mask_win(:,ind) = mask(:,ind);
     
-    % get corrected column
-    valeq(:,i) = val_win(:, width+1);
+    % lookup table (empirical cdf for sand pixels in window)
+    [new, old] = ecdf(val(mask_win));
     
+    % correct using lookup table
+    valeq(mask_col) = interp1(old(2:end), new(2:end), val(mask_col));
 end
-    
+
+% add a tiny offset so that no sand pixels are exactly zero
+valeq(mask & valeq==0) = valeq(mask & valeq==0)+1e-5;
