451da7699bf48bbddc55efa2ac383977eb8e95f0
keithma <keithma@bu.edu>
2015-07-29 13:17:46 -0500

    Cleaned up histogram equalization code

--- preprocess/correct_color_eq.m
+++ preprocess/correct_color_eq.m
@@ -5,20 +5,23 @@ hsv = rgb2hsv(rgb);
 val = hsv(:,:,3);
 [nrow, ncol] = size(val);
  
-
 % equalize histogram, column-by-column
 valeq = zeros([nrow, ncol]);
 for i = 1:ncol
     
+    % window indices, with symetric padding
     ind = (i-width):(i+width);
     ind(ind<=0) = abs(ind(ind<=0))+2;
     ind(ind>ncol) = 2*ncol-ind(ind>ncol);
     
-     val_win = val(:, ind);
-     mask_win = mask(:, ind);
-     eql = histeq(val_win(mask_win));
-     val_win(mask_win) = eql;
-     valeq(:,i) = val_win(:, width+1);
+    % extract sand pixels in window and equalize
+    val_win = val(:, ind);
+    mask_win = mask(:, ind);
+    eql = histeq(val_win(mask_win));
+    val_win(mask_win) = eql;
+    
+    % get corrected column
+    valeq(:,i) = val_win(:, width+1);
+    
 end
-keyboard
     
