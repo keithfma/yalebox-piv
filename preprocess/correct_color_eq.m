ddf1094b85e621160e5bc9ef96688dc4623888d3
keithma <keithma@bu.edu>
2015-07-29 17:08:15 -0500

    Added help to new color correction code

--- preprocess/correct_color_eq.m
+++ preprocess/correct_color_eq.m
@@ -1,4 +1,40 @@
-function [valeq] = correct_color_eq(rgb, mask, width)
+function [eql] = correct_color_eq(rgb, mask, width)
+% function [eql] = correct_color(rgb, mask, width)
+%
+% Compute and apply local histogram-equalization color corrections.
+% Corrections are computed for each column based on the histogram for sand
+% pixels within "width" columns. This sliding-window approach corrects for
+% lighting gradients in the x-direction (but not the y-direction). For
+% details on the equalization algorithm, see:
+%
+% http://fourier.eng.hmc.edu/e161/lectures/contrast_transform/node2.html
+%
+% All arguments are required - default values are used where inputs are []
+%
+% Arguments:
+%
+%   rgb = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
+%       MATLAB with imread()
+%
+%   mask = 2D matrix, double, TRUE where there is sand, FALSE elsewhere
+%
+%   width = Scalar, double, half-width of the sliding window for quantile
+%       calculations, in (whole) pixels, default = 50; 
+%
+% Keith Ma, July 2015
+
+% set defaults
+if isempty(width); width = 50; end
+
+% check for sane inputs
+assert(isa(rgb, 'uint8') && size(rgb,3) == 3, ...
+     'rgb is not a Truecolor (24-bit) RGB image');
+assert(isa(mask, 'logical'), ...
+    'mask is not of type logical ');
+assert(size(mask,1) == size(rgb, 1) && size(mask,2) == size(rgb, 2), ...
+    'mask and rgb dimensions do not match');
+assert(numel(width) == 1 && mod(width,1) == 0, ...
+    'width is not a scalar integer');
 
 % convert image to 2D intensity matrix
 hsv = rgb2hsv(rgb);
@@ -6,7 +42,7 @@ val = hsv(:,:,3);
 [nrow, ncol] = size(val);
 
 % equalize histogram, column-by-column
-valeq = zeros([nrow, ncol]);
+eql = zeros([nrow, ncol]);
 for i = 1:ncol
     
     % window indices, with symetric padding
@@ -24,8 +60,8 @@ for i = 1:ncol
     [new, old] = ecdf(val(mask_win));
     
     % correct using lookup table
-    valeq(mask_col) = interp1(old(2:end), new(2:end), val(mask_col));
+    eql(mask_col) = interp1(old(2:end), new(2:end), val(mask_col));
 end
 
 % add a tiny offset so that no sand pixels are exactly zero
-valeq(mask & valeq==0) = valeq(mask & valeq==0)+1e-5;
+eql(mask & eql==0) = eql(mask & eql==0)+1e-5;
