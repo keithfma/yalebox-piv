c5721eace29959d4b6f1b2a8fc1d88c414472f13
keithma <keithma@bu.edu>
2015-07-30 15:12:00 -0500

    Protected against user error while entering world coordinates

--- yalebox_prep_world_coord.m
+++ yalebox_prep_world_coord.m
@@ -14,10 +14,13 @@ function [x, y] = yalebox_prep_world_coord(woco_image)
 % Keith Ma, July 2015
 
 % check for sane arguments
+
+% display coordinate grid image
 im = imread(woco_image);
+imshow(im)
+hold on
 
 % select control points interactively - minimum of 4 points, no maximum
-imshow(im)
 ctrl_pt_obj = [impoint(), impoint(), impoint()];
 while 1
     ctrl_pt_obj(end+1) = impoint();
@@ -34,25 +37,28 @@ for i = 1:npts
     delete(ctrl_pt_obj(i));
 end
 
-% MAKE THIS A FINITE WHILE LOOP, ONLY INCREMENT IF THERE IS NO ERROR
-
 % review control points and enter positions
 ctrl_pt_world = nan(npts, 2);
-hold on
-for i = 1:npts
+i = 1;
+while i <= npts
     
     % request x, y position from user
     this = plot(ctrl_pt_image(i,1), ctrl_pt_image(i,2), '*r');
-    tmp = inputdlg({'X', 'Y'}, 'Input world coordinates in meters as X, Y');
-    ctrl_pt_world(i, :) = str2num(cell2mat(tmp));
+    try 
+        tmp = inputdlg({'X', 'Y'}, 'Input world coordinates in meters as X, Y');
+        delete(this);
+        ctrl_pt_world(i, :) = str2num(cell2mat(tmp));
+    catch
+        continue
+    end
     
-    % update display
-    delete(this);
+    % next point
     plot(ctrl_pt_image(i,1), ctrl_pt_image(i,2), '*k');
     text(ctrl_pt_image(i,1), ctrl_pt_image(i,2), ...
         sprintf('%.2f, %.2f', ctrl_pt_world(i,1), ctrl_pt_world(i,2)),...
         'VerticalAlignment', 'Bottom',...
         'HorizontalAlignment', 'Center');
+    i = i+1;
 end
 
 
