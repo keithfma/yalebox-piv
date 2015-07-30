2a86ede884f9f6d65abe956d8a43b55a0034f493
keithma <keithma@bu.edu>
2015-07-30 17:31:19 -0500

    complete: Updated arguments and added help

--- yalebox_prep_world_coord.m
+++ yalebox_prep_world_coord.m
@@ -1,13 +1,26 @@
-function [x, y] = yalebox_prep_world_coord(woco_image, npts, show)
+function [x, y, prm] = yalebox_prep_world_coord(woco_image, npts, show)
+% function [x, y, prm] = yalebox_prep_world_coord(woco_image, npts, show)
 %
+% Compute the best-fit cartesian coordinate system for the coordinate grid
+% image "woco_image". As a result of previous preprocessing steps (i.e.
+% distortion correction and cropping), the coordinate system is neither
+% rotated or skew. This means that the coordinate transformation can be
+% descrbed with four free parameters, and the x- and y-direction transforms
+% are independent. Specifically, the transformation is:
+%
+%   x_world = x_pixel*x_scale + x_offset
+%   y_world = y_pixel*y_scale + y_offset
 %
 % Arguments:
 %
-%   woco_image = String, filename of the world coordinate grid image.
+%   woco_image = String, filename of the world coordinate grid image,
+%       required.
 %
-%   npts = Integer, number of control points to define 
+%   npts = Integer, number of control points to define, must be >= 4 to
+%       provide sufficient infomation to fit four coordinate transformation
+%       parameters, default = 4
 %
-%   show = True to display image in world coordinates
+%   show = True to display image in world coordinates, default = true
 %
 %   x = 1D vector, length == number of columns in coordinage grid image,
 %       double, world coordinate x-position in meters 
@@ -15,10 +28,30 @@ function [x, y] = yalebox_prep_world_coord(woco_image, npts, show)
 %   y = 1D vector, length == number of rows in coordinage grid image,
 %       double, world coordinate y-position in meters 
 %
+%   prm = Struct, contains all of the parameters (input and internally
+%       computed), included so that default values can be recovered.
+%       Structure member varaibles are:
+%       
+%       prm.woco_image
+%       prm.npts
+%       prm.x_scale
+%       prm.x_offset
+%       prm.y_scale
+%       prm.y_offset
+%
 % Keith Ma, July 2015
 
+% set defaults
+if isempty(npts); npts = 4; end
+if isempty(show); show = true; end
+
 % check for sane arguments
-% npts >= 4
+assert(isa(woco_image, 'char') && exist(woco_image, 'file') == 2, ...
+    'woco_image is not a valid filename');
+assert(numel(npts) == 1 && mod(npts, 1) == 0 && npts >= 4, ...
+    'npts is not a scalar integer greater than 4.');
+assert(numel(show) == 1 && (show == 1 || show == 0), ...
+    'show is not a scalar logical flag.');
 
 % display coordinate grid image
 im = imread(woco_image);
@@ -99,3 +132,12 @@ if show
     xlabel('X, meters');
     ylabel('Y, meters');
 end
+
+% prepare parameter struct
+prm.woco_image = woco_image;
+prm.npts = npts;
+prm.x_scale = x_scale;
+prm.x_offset = x_offset;
+prm.y_scale = y_scale;
+prm.y_offset = y_offset;
+
