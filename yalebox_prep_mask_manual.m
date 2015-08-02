c9adf47cffcc4ab8a0a99b906c6902eac61fa9ff
keithma <keithma@bu.edu>
2015-08-02 07:56:27 -0500

    Modified manual masking to take intensity image also

--- yalebox_prep_mask_manual.m
+++ yalebox_prep_mask_manual.m
@@ -1,12 +1,12 @@
-function mask_manual = yalebox_prep_mask_manual(rgb)
-% function mask_manual = yalebox_prep_mask_manual(rgb)
+function mask_manual = yalebox_prep_mask_manual(img)
+% function mask_manual = yalebox_prep_mask_manual(img)
 %
 % Interactively create a mask to black out region(s) of an image.
 %
 % Arguments:
 %
-%   rgb = 3D matrix, uint8, a 24-bit "Truecolor" RGB image, as read into
-%       MATLAB with imread()
+%   img = Variable format, either 3D matrix, uint8, a 24-bit "Truecolor"
+%       RGB image, or 2D matrix, double, range [0,1] intensity image
 %
 %   mask = 2D matrix, logical, true where there is sand and false
 %       elsewhere.
@@ -14,15 +14,16 @@ function mask_manual = yalebox_prep_mask_manual(rgb)
 % Keith Ma, July 2015
 
 % check for sane arguments
-assert(isa(rgb, 'uint8') && size(rgb,3) == 3, ...
-    'img_rgb is not a 24-bit RGB image');
+assert( (isa(img, 'uint8') && size(img,3) == 3) | ...
+        (isa(img, 'double') && ismatrix(img)), ...
+     'img_rgb is not a 24-bit RGB image');
 
 % initialize mask and figure
-mask = false(size(rgb,1), size(rgb,2));
-rgbm = rgb;
+mask = false(size(img,1), size(img,2));
+imgm = img;
 figure()
 subplot(2,1,1)
-imshow(rgb); title('original');
+imshow(img); title('original');
 subplot(2,1,2); title('masked'); 
 
 % add polygons to mask interactively
@@ -34,9 +35,9 @@ while 1
     this_mask = createMask(p);
     
     % apply mask and display
-    rgbm(repmat(this_mask, 1, 1, 3)) = 0;
+    imgm(repmat(this_mask, 1, 1, size(img, 3))) = 0;
     subplot(2,1,2)
-    imshow(rgbm);
+    imshow(imgm);
     
     % query user
     choice = input('Keep (1) or discard (anything else): ');
