93d9d8531d05fdad2b28f294cbe2436169228ec3
keithma <keithma@bu.edu>
2015-08-04 23:01:13 -0400

    Re-implemented intensity correction using MATLAB's adapthisteq
    Note that this version does not ignore non-sand pixels.

--- yalebox_prep_intensity.m
+++ yalebox_prep_intensity.m
@@ -1,13 +1,9 @@
-function img = yalebox_prep_intensity(rgb, mask, histeq_width, show)
-% function img = yalebox_prep_intensity(rgb, mask, histeq_width, show)
+function img = yalebox_prep_intensity(rgb, mask, num_tiles, show)
+% function img = yalebox_prep_intensity(rgb, mask, show)
 %
-% Compute and apply local histogram-equalization color corrections.
-% Corrections are computed for each column based on the histogram for sand
-% pixels within "histeq_width" columns. This sliding-window approach corrects for
-% lighting gradients in the x-direction (but not the y-direction). For
-% details on the equalization algorithm, see:
-%
-% http://fourier.eng.hmc.edu/e161/lectures/contrast_transform/node2.html
+% Apply local histogram-equalization color correction, ignoring non-sand
+% pixels. This is done using a modified version of MATLAB's adapthisteq()
+% tool, which is included below. See "help adapthisteq" for more details.
 %
 % Arguments:
 %
@@ -16,8 +12,10 @@ function img = yalebox_prep_intensity(rgb, mask, histeq_width, show)
 %
 %   mask = 2D matrix, double, TRUE where there is sand, FALSE elsewhere
 %
-%   histeq_width = Scalar, double, half-width of the sliding window for quantile
-%       calculations, in (whole) pixels.
+%   num_tiles = 2-element vector, double, parameter to adapthisteq. From
+%       the MATLAB help: "Two-element vector of positive integers specifying
+%       the number of tiles by row and column, [M N]. Both M and N must be at
+%       least 2. The total number of tiles is equal to M*N."
 %   
 %   show = Optional, scalar, logical flag, set to True to plot the original and
 %       equalized intensity images, default = false
@@ -34,41 +32,29 @@ assert(isa(mask, 'logical'), ...
     'mask is not of type logical ');
 assert(size(mask,1) == size(rgb, 1) && size(mask,2) == size(rgb, 2), ...
     'mask and rgb dimensions do not match');
-assert(numel(histeq_width) == 1 && mod(histeq_width,1) == 0, ...
-    'histeq_width is not a scalar integer');
-if nargin == 3; show = false; end
+assert(numel(num_tiles) == 2 & ...
+    mod(num_tiles(1), 1) == 0 & ...
+    mod(num_tiles(2), 1) == 0, ...
+    'num_tiles is not a two-element vector of integers.');
+if nargin == 5; show = false; end
 assert(numel(show) == 1 && (show == 0 || show == 1), ...
     'show is not a logical flag');
+
+% pre-defined parameters
+clip_limit = 1;
+num_bins = 1e4;
 
 % convert image to 2D intensity matrix
 hsv = rgb2hsv(rgb);
 val = hsv(:,:,3);
-[nrow, ncol] = size(val);
 
-% equalize histogram, column-by-column
-img = zeros([nrow, ncol]);
-for i = 1:ncol
+% local histogram equalization (CLAHE) ignoring non-sand pixels
+img = adapthisteq(val, ...
+        'NumTiles', num_tiles, ...
+        'NBins', num_bins, ...
+        'ClipLimit', clip_limit, ...
+        'Range', 'full');
     
-    % window indices, with symetric padding
-    ind = (i-histeq_width):(i+histeq_width);
-    ind(ind<=0) = abs(ind(ind<=0))+2;
-    ind(ind>ncol) = 2*ncol-ind(ind>ncol);
-    
-    % mask for sand pixels in col and in window
-    mask_col = false(size(mask));
-    mask_col(:,i) = mask(:,i);
-    mask_win = false(size(mask));
-    mask_win(:,ind) = mask(:,ind);
-    
-    % correct using lookup table
-    sand = val(mask_win);
-    if ~isempty(sand)
-        [new, old] = ecdf(sand);
-        img(mask_col) = interp1(old(2:end), new(2:end), val(mask_col));
-    end
-    
-end
-
 % add a tiny offset so that no sand pixels are exactly zero
 tiny = 1e-5;
 img(mask & img==0) = img(mask & img==0)+tiny;
@@ -84,5 +70,5 @@ if show
     title('equalized intensity')
     imagesc(img)
     caxis([0,1]);
+    colormap(gray)
 end
-
