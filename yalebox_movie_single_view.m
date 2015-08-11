e34d53eae7725f1df44baf6d35af7a41520b31ef
keithma <keithma@bu.edu>
2015-08-11 16:12:56 -0400

    single_view: working s-point triangle annotation

--- yalebox_movie_single_view.m
+++ yalebox_movie_single_view.m
@@ -8,6 +8,10 @@ function [] = yalebox_movie_single_view(input_file, output_file)
 % Keith Ma, August 2015
 
 % define parameters
+len_tri = 0.01; % m
+color_tri = 'yellow';
+opacity = 0.7;
+
 
 % get netcdf ids
 ncid = netcdf.open(input_file, 'NOWRITE');
@@ -20,9 +24,25 @@ intens_id = netcdf.inqVarID(ncid, 'intensity');
 x = netcdf.getVar(ncid, x_id);
 y = netcdf.getVar(ncid, y_id);
 step = netcdf.getVar(ncid, step_id);
-nx = numel(x);
-ny = numel(y);
+nx = numel(x); dx = abs(x(1)-x(2));
+ny = numel(y); dy = abs(y(1)-y(2));
 ns = numel(step);
+
+% get shape annotations
+
+spt_r = interp1(y, 1:ny, 0, 'linear', 'extrap');
+spt_c = interp1(x, 1:nx, 0, 'linear', 'extrap');
+if abs(spt_r-ny) < abs(spt_r) % spt at image top
+    pos_tri = [spt_c, spt_r, ...
+               spt_c+len_tri/2/dx, spt_r-sqrt(3)/2*len_tri/dy, ...
+               spt_c-len_tri/2/dx, spt_r-sqrt(3)/2*len_tri/dy];
+else % spt at image bottom
+end
+
+shape_pos = {pos_tri};
+shape_color = {color_tri};
+
+
 
 % % prepare movie object: 16-bit grayscale Motion JPEG 2000, lossless compression
 % movie_writer = VideoWriter(output_file, 'Archival');
@@ -35,8 +55,11 @@ ns = numel(step);
 for i = 1    
     % read intensity image
     intens = netcdf.getVar(ncid, intens_id, [0, 0, i-1], [nx, ny, 1])';
-    
+        
     % add annotations using insertShape and similar
+    intens = insertShape(intens, 'FilledPolygon', shape_pos, ...
+        'Color', shape_color, 'Opacity', opacity);
+    
     
     % create frame    
 end
