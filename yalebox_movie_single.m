29883202f5951ee636f93017b49c29a7ec6321ee
keithma <keithma@bu.edu>
2015-08-11 21:45:11 -0400

    movie_single: Added new resizing function
    This has the dual benefit of resizing the movie before it is written, and
    circumventing the obnoxious maximum font size in the insertText function
    (smaller image means the font appears larger).

--- yalebox_movie_single.m
+++ yalebox_movie_single.m
@@ -1,4 +1,4 @@
-function [] = yalebox_movie_single_view(input_file, output_file)
+function [] = yalebox_movie_single(input_file, output_file)
 %
 % input_file = String, path to input netCDF file as produced by
 %   yalebox_prep_create_input().
@@ -13,7 +13,7 @@ tri_len = 0.01; % m
 title_str = 'Party Time';
 
 % internal parameters
-movie_max_dim = [1920, 1080];
+max_mov_dim = [1920, 1080];
 
 tri_color = 'red';
 tri_opacity = 0.7;
@@ -25,15 +25,12 @@ title_box_opacity = 1;
 
 % get netcdf ids
 ncid = netcdf.open(input_file, 'NOWRITE');
-x_id = netcdf.inqVarID(ncid, 'x');
-y_id = netcdf.inqVarID(ncid, 'y');
-step_id = netcdf.inqVarID(ncid, 'step');
 intens_id = netcdf.inqVarID(ncid, 'intensity');
 
 % get coordinate vectors
-x = netcdf.getVar(ncid, x_id);
-y = netcdf.getVar(ncid, y_id);
-step = netcdf.getVar(ncid, step_id);
+x = netcdf.getVar(ncid, netcdf.inqVarID(ncid, 'x'));
+y = netcdf.getVar(ncid, netcdf.inqVarID(ncid, 'y'));
+step = netcdf.getVar(ncid, netcdf.inqVarID(ncid, 'step'));
 
 % % make sure image top is at row 1
 % if y(1) < y(end)
@@ -58,14 +55,11 @@ for i = 1
     % read intensity image
     intens = netcdf.getVar(ncid, intens_id, [0, 0, i-1], [numel(x), numel(y), 1])';
     
-    % downscale and flip if needed
-    frame = yalebox_movie_aux_resize(intens, x, y, max_mov_dim);
-    
-    scale = min([numel(x), numel(y)]./movie_max_dim);
-    
+    % resize and reshape image as needed
+    [frame, xf, yf] = yalebox_movie_aux_resize(intens, x, y, max_mov_dim);
     
     % add annotations (triangles, scale, title, counter)
-    frame = yalebox_movie_aux_add_spt(intens, x, y, tri_tip, tri_len, ...
+    frame = yalebox_movie_aux_add_spt(frame, xf, yf, tri_tip, tri_len, ...
         tri_color, tri_opacity);
     frame = yalebox_movie_aux_add_title(frame, title_str, title_font_size, ...
         title_font_color, title_box_color, title_box_opacity);
