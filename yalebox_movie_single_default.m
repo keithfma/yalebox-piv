0dc1ec66cf3dcc2f3d8e6942ae1a6045d24480d3
keithma <keithma@bu.edu>
2015-08-12 23:55:02 -0400

    movie_single: added skeleton for default function

---
+++ yalebox_movie_single_default.m
@@ -0,0 +1,12 @@
+function prm = yalebox_movie_single_default()
+%
+% Creates a struct containing all the parameters needed by
+% yalebox_movie_single(), populated with default values. Typical usage
+% would be to call this function to get a properly formated parameter
+% struct, edit the parameters as needed, then pass the edited struct to
+% yalebox_movie_single() to make the movie.
+%
+% Keith Ma, August 2015
+
+% debug
+prm = 0;
