50b33afc155e779c31f97f8686645b05275775f6
keithma <keithma@bu.edu>
2015-09-24 22:07:53 -0400

    Simplified test datasets and dir structure

--- test/yalebox_test_maskN_eqlN_normN_cbcN.m
+++ test/yalebox_test_maskN_eqlN_normN_cbcN.m
@@ -3,6 +3,17 @@
 % Masked = N
 % Equalized histogram = N
 % Use normalized xcorr = N
-% Use CBC = N:w
+% Use CBC = N
+
+function [xx, yy, uu, vv] = yalebox_piv_step(...
+                                ini, fin, xx, yy, npass, samplen, sampspc, ...
+         
+% define parameters
+ini_file_1 = 'fault_ss_01_sidef_030.png';
+fin_file_1 = 'fault_ss_01_sidef_031.png';
+
+ini_file_1 = 'fault_ss_01_sidef_250.png';
+fin_file_1 = 'fault_ss_01_sidef_250.png';
+
 
 
