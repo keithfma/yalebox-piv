7e93514bbbd6431cd901d972907288f9e255a4e5
keithma <keithma@bu.edu>
2015-09-29 13:04:40 -0400

    Completed function for synthetic constant offset
    Included one template image in the repo as well

--- test/synth/create/create_constant_uv.m
+++ test/synth/create/create_constant_uv.m
@@ -1,4 +1,4 @@
-function [] = create_constant_uv(template, uu, vv, name)
+function [ini, fin, xx, yy] = create_constant_uv(template, uu, vv, show)
 % Create a test dataset with constant x-dir and y-dir offsets from a template
 % sand image. Outputs are cropped, grayscale versions of the template image.
 %
@@ -13,15 +13,69 @@ function [] = create_constant_uv(template, uu, vv, name)
 %       in pixels, must be an integer (to avoid introducing noise by
 %       interpolation).
 %
-%   name = String, stub for the output image pair file names, e.g. 'my_img'
-%       would yield the output files my_img_1.png, my_img_2.png
+%   show = Scalar, logical, flag enabling or disabling display of the test image
+%       pair. If enabled, the program enters an infinite loop, alternately
+%       displaying one image and the other until the user terminates via the
+%       Ctrl+C command or closes the figure window.
+% %
 
-% Check for sane inputs
+% check for sane inputs
 validateattributes(template, {'char'}, {'vector'}, ...
     'create_constant_uv', 'template');
 validateattributes(uu, {'numeric'}, {'scalar', 'integer'}, ...
     'create_constant_uv', 'uu');
 validateattributes(uu, {'numeric'}, {'scalar', 'integer'}, ...
     'create_constant_uv', 'vv');
-validateattributes(name, {'char'}, {'vector'}, ...
-    'create_constant_uv', 'name');
+validateattributes(show, {'numeric', 'logical'}, {'binary'}, ... 
+    'create_constant_uv', 'show');
+
+% read template data to intensity matrix
+ii = imread(template);
+ii = rgb2hsv(ii);
+ii = ii(:, :, 3);
+
+% apply displacements by cropping the template
+if uu > 0
+    ini = ii(:, uu+1:end);
+    fin = ii(:, 1:end-uu);
+elseif uu < 0 
+    ini = ii(:, 1:end+uu);
+    fin = ii(:, -uu+1:end);       
+else
+    ini = ii;
+    fin = ii;    
+end
+
+if vv > 0
+    ini = ini(vv+1:end, :);
+    fin = fin(1:end-vv, :);
+elseif vv < 0 
+    ini = ini(1:end+vv, :);
+    fin = fin(-vv+1:end, :);       
+else
+     % do nothing 
+end
+
+% display image pair
+if show
+    clr = [min(ii(:)), max(ii(:))];
+    figure;
+    while 1
+        imagesc(ini);
+        set(gca, 'YDir', 'normal')        
+        set(gcf, 'Name', 'ini');
+        caxis(clr);
+        pause(1)
+        imagesc(fin); 
+        set(gca, 'YDir', 'normal')
+        set(gcf, 'Name', 'fin');
+        caxis(clr);
+        pause(1)
+    end
+end
+
+
+
+
+
+
