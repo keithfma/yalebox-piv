499f56193ad9fb73b9fd83200609b88009c2a961
keithma <keithma@bu.edu>
2015-10-16 16:30:34 -0400

    Skeleton for tangental velocity padding routine

---
+++ exp/pad_tangental.m
@@ -0,0 +1,13 @@
+% Script. Apply pad to match tangental velocities outside the wedge with
+% the surface velocities.
+%
+% Uses a nearest-neighbor interpolation to extend the intensity values
+% along the surface of the wedge outwards along normal vectors. Since these
+% edge pixels are translated along the wedge boundary, they constrain the
+% tangental velocity.
+
+% load image pair data (masked)
+
+% create pad
+
+% visualize results
