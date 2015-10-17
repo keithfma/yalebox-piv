0391da49f480a7866e0c6509de708115eff359b1
keithma <keithma@bu.edu>
2015-10-17 18:15:21 -0400

    Completed simple tangental pad (nearest neighbor)
    Performs poorly. Resructured program to test alternate methods.

--- exp/pad_tangental.m
+++ exp/pad_tangental.m
@@ -6,8 +6,69 @@
 % edge pixels are translated along the wedge boundary, they constrain the
 % tangental velocity.
 
-% load image pair data (masked)
+%% load image pair data, get masks, pad edges
 
-% create pad
+% parameters
+input_file = 'fault_ss_01_sidef_250_251.mat';
+npad = 50;
 
-% visualize results
+% run
+load(input_file, 'ini', 'fin'); 
+
+ini = padarray(ini, [npad, npad], 0, 'both');
+fin = padarray(fin, [npad, npad], 0, 'both');
+
+ini_mask = ini~=0;
+fin_mask = fin~=0;
+
+%% create pad: simple nearest neighbor
+% -> too much noise on the boundary, will not be useful
+on = 1; 
+if on 
+
+    % run
+    [ini_dist, ini_nearest] = bwdist(ini_mask);
+    ini_pad = ini(nearest(ini_nearest));
+    
+    [fin_dist, fin_nearest] = bwdist(fin_mask);
+    fin_pad = fin(nearest(fin_nearest));
+    
+end
+%% create pad: nearest neighbor with a skin depth
+%
+on = 0;
+if on
+    
+    
+    skin_depth = 5;
+    
+    ini_pad_0 = ini;
+    
+    for ii = 0:skin_depth-1
+        
+        
+        
+        
+    end
+
+
+end
+%% visualize results
+
+% parameters
+clim = [0, 0.4];
+
+% run
+figure;
+while 1
+    imagesc(ini_pad);
+    caxis(clim);
+    axis equal
+    axis tight
+    pause(1);    
+    imagesc(fin_pad);
+    caxis(clim);
+    axis equal
+    axis tight
+    pause(1);
+end
