b48e11f355c2e1e60762283081ccf0f3b13cd6e3
keithfma <keithfma@gmail.com>
2015-12-08 09:25:30 -0500

    Created skeleton for synthetic dots test

---
+++ test/synth/create_dots.m
@@ -0,0 +1,18 @@
+function [ini, fin, xx, yy, uu, vv] = create_dots()
+%
+% Create a synthetic image pair that consists of a random field of gaussian dots
+% truncate by a boundary, which is subjected to a constant translation +
+% homogenous deformation.
+%
+% Arguments:
+%
+% %
+
+% debug: assign dummy outputs {
+ini = [];
+fin = [];
+xx = [];
+yy = [];
+uu = [];
+vv = [];
+% } debug
