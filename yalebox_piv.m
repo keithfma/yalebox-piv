f61d5b243c728fbe2eb8cd42bc037e5d06b48830
keithfma <keithfma@gmail.com>
2016-02-01 23:16:06 -0500

    New validation routine using normalized median on N nearest neighbors

--- yalebox_piv.m
+++ yalebox_piv.m
@@ -158,7 +158,8 @@ for pp = 1:np
     end 
     
     % validate dislacement updates 
-    [du_pts_tm, dv_pts_tm] = piv_validate_pts_nmed(c_pts, r_pts, du_pts_tm, dv_pts_tm, sampspc(pp)*2, valid_max, valid_eps, true);    
+    keyboard
+    [du_pts_tm, dv_pts_tm] = piv_validate_pts_nmed_b(c_pts, r_pts, du_pts_tm, dv_pts_tm, 8, valid_max, valid_eps, true);    
     
     % interpolate displacement update to sample grid, points outside roi remain NaN
     from = ~isnan(du_pts_tm); 
