9fd5614bc0509795d78c2849051754e8e097a1c4
Keith Ma <keithma@bu.edu>
2016-02-19 00:23:41 -0500

    Minor changes to get prep() batch submission working

--- batch_prep_scc.sh
+++ batch_prep_scc.sh
@@ -8,24 +8,22 @@
 
 # define parameters
 yalebox_path=/projectnb/glaciermod/yalebox-piv/
-param_file=/projectnb/glaciermod/sandbox-tmp/prep/fault_ss_01_siden_param.mat
-input_file_work=$TMPDIR/tmp.nc
-input_file_dest=/projectnb/glaciermod/sandbox-tmp/prep/fault_ss_01_siden_in.nc
-image_path=/projectnb/glaciermod/sandbox-tmp/fault_ss_01/siden/
-image_wild=$image_path'fault_ss_01_siden_*.png'
+param_file=/projectnb/glaciermod/yalebox-exp-fault/data/fault_ss_01/piv/test/prep_fault_ss_01_sidef.mat
+output_file_work=/projectnb/glaciermod/yalebox-exp-fault/data/fault_ss_01/piv/test/tmp.nc
+output_file_dest=/projectnb/glaciermod/yalebox-exp-fault/data/fault_ss_01/piv/test/out.nc
+image_path=/projectnb/glaciermod/yalebox-exp-fault/data/fault_ss_01/piv/test/
+image_wild=$image_path'fault_ss_01_sidef_*.png'
 
 # setup runtime environment
-module load matlab/2014b
+module load matlab/2015b
 export MATLABPATH=$yalebox_path:$MATLABPATH
 
 # run
-matlab -singleCompThread -nodisplay << EOF
+matlab -singlecompthread -nodisplay << EOF
 load $param_file
 d = dir('$image_wild'); image_names = {d.name};
-prep('$input_file_work', '$image_path', image_names, ...
-  x, y, scale, offset, mask_manual, hue_lim, val_lim, entr_lim, entr_win, ...
-  morph_rad, num_tiles);
-movefile('$input_file_work', '$input_file_dest');
+prep('$output_file_work', '$image_path', image_names, x, y, scale, offset, mask_manual, hue_lim, val_lim, entr_lim, entr_win, morph_open_rad, morph_erode_rad, nwin);
+movefile('$output_file_work', '$output_file_dest');
 exit
 EOF
 
