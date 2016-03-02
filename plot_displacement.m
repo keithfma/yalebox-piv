9a90e9b5d667d3c58b4e00e55275df7cf14896a3
keithfma <keithfma@gmail.com>
2016-03-02 00:49:41 -0500

    plot_displacement: added default arguments, updated variable names

--- plot_displacement.m
+++ plot_displacement.m
@@ -1,4 +1,4 @@
-function [] = plot_displacement(piv_file, index, xlimits, ylimits, ulimits, vlimits, mlimits)
+function [] = plot_displacement(piv_file, index, xlim, ylim, ulim, vlim, mlim)
 %
 % Plot displacement magnitude and direction. Function generates 3 separate plots:
 %   - x-direction displacement 
@@ -14,28 +14,36 @@ function [] = plot_displacement(piv_file, index, xlimits, ylimits, ulimits, vlim
 % index = Scalar, index of timestep in piv_file to plot, uses MATLAB-style
 %   1-based indices.
 %
-% xlimits, ylimits, ulimits, vlimits, mlimits = Vector, length==2, [minimum, maximum] values for the x-axis, y-axis, u-displacement, v-displacement, and
+% xlim, ylim, ulim, vlim, mlim = Vector, length==2, [minimum, maximum] values for the x-axis, y-axis, u-displacement, v-displacement, and
 %   displacement magnitude. Values beyond the range of the data will be
 %   truncated to the data limits. Thus, to span the data, one could use [-inf, inf].
 % %
 
+% set defaults
+if nargin < 3 || isempty(xlim); xlim = [-inf, inf]; end
+if nargin < 4 || isempty(ylim); ylim = [-inf, inf]; end
+if nargin < 5 || isempty(ulim); ulim = [-inf, inf]; end
+if nargin < 6 || isempty(vlim); vlim = [-inf, inf]; end
+if nargin < 7 || isempty(mlim); mlim = [-inf, inf]; end
+
 % debug: hard-code input arguments
 piv_file = '~/Documents/dissertation/yalebox-exp-fault/data/fault_ss_01/piv/fault_ss_01_sidef.displ.nc';
 index = 100;
-xlimits = [-inf, inf];
-ylimits = [-inf, 0.1];
-ulimits = [-inf, inf];
-vlimits = [0, inf];
-mlimits = [-inf, inf];
+xlim = [-inf, inf];
+ylim = [-inf, 0.1];
+ulim = [-inf, inf];
+vlim = [-inf, inf];
+mlim = [-inf, inf];
+% end debug
 
 % check for sane inputs
 validateattributes(piv_file, {'char'}, {'vector'});
 validateattributes(index, {'numeric'}, {'scalar', 'positive', 'integer'});
-validateattributes(xlimits, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
-validateattributes(ylimits, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
-validateattributes(ulimits, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
-validateattributes(vlimits, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
-validateattributes(mlimits, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
+validateattributes(xlim, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
+validateattributes(ylim, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
+validateattributes(ulim, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
+validateattributes(vlim, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
+validateattributes(mlim, {'numeric'}, {'vector', 'numel', 2, 'increasing'});
 
 % read data from netCDF
 xx = ncread(piv_file, 'x'); nx = numel(xx);
@@ -46,28 +54,28 @@ vv = squeeze(ncread(piv_file, 'v', [1, 1, index], [nx, ny, 1]))';
 mm = sqrt(uu.*uu+vv.*vv);
 
 % truncate axis limits
-xlimits(1) = max(xlimits(1), min(xx)); xlimits(2) = min(xlimits(2), max(xx));
-ylimits(1) = max(ylimits(1), min(yy)); ylimits(2) = min(ylimits(2), max(yy));
-ulimits(1) = max(ulimits(1), min(uu(:))); ulimits(2) = min(ulimits(2), max(uu(:)));
-vlimits(1) = max(vlimits(1), min(vv(:))); vlimits(2) = min(vlimits(2), max(vv(:)));
-mlimits(1) = max(mlimits(1), min(mm(:))); mlimits(2) = min(mlimits(2), max(mm(:)));
+xlim(1) = max(xlim(1), min(xx)); xlim(2) = min(xlim(2), max(xx));
+ylim(1) = max(ylim(1), min(yy)); ylim(2) = min(ylim(2), max(yy));
+ulim(1) = max(ulim(1), min(uu(:))); ulim(2) = min(ulim(2), max(uu(:)));
+vlim(1) = max(vlim(1), min(vv(:))); vlim(2) = min(vlim(2), max(vv(:)));
+mlim(1) = max(mlim(1), min(mm(:))); mlim(2) = min(mlim(2), max(mm(:)));
 
 % convert units
 xunits = '[cm]';
 xx = xx*100; % m -> cm
-xlimits = xlimits*100;
+xlim = xlim*100;
 
 yunits = '[cm]';
 yy = yy*100; % m -> cm
-ylimits = ylimits*100;
+ylim = ylim*100;
 
 cunits = '[mm/step]';
 uu = uu*1000; % m -> mm
 vv = vv*1000;
 mm = mm*1000;
-ulimits = ulimits*1000;
-vlimits = vlimits*1000;
-mlimits = mlimits*1000;
+ulim = ulim*1000;
+vlim = vlim*1000;
+mlim = mlim*1000;
 
 % create figures and axes
 [ax_top, ax_mid, ax_bot] = create_figure();
@@ -75,19 +83,19 @@ mlimits = mlimits*1000;
 % plot displacement magnitude and direction
 axes(ax_top)
 imagesc(xx, yy, mm, 'AlphaData', ~isnan(mm)); 
-format_axes(gca, xlimits, xunits, 0, ylimits, yunits, 1, mlimits, cunits, ...
+format_axes(gca, xlim, xunits, 0, ylim, yunits, 1, mlim, cunits, ...
     sprintf('displacement vector magnitude, step = %.1f', step));
 
 % plot x-direction displacement magnitude
 axes(ax_mid);
 imagesc(xx, yy, uu, 'AlphaData', ~isnan(uu)); 
-format_axes(gca, xlimits, xunits, 0, ylimits, yunits, 1, ulimits, cunits, ...
+format_axes(gca, xlim, xunits, 0, ylim, yunits, 1, ulim, cunits, ...
     sprintf('x-direction displacement component, step = %.1f', step));
 
 % plot y-direction displacement magnitude
 axes(ax_bot);
 imagesc(xx, yy, vv, 'AlphaData', ~isnan(vv)); 
-format_axes(gca, xlimits, xunits, 1, ylimits, yunits, 1, vlimits, cunits, ...
+format_axes(gca, xlim, xunits, 1, ylim, yunits, 1, vlim, cunits, ...
     sprintf('y-direction displacement component, step = %.1f', step));
 
 keyboard
@@ -120,14 +128,14 @@ top = axes('Position', top_position);
 
 end
 
-function [] = format_axes(ax, xlimits, xunits, xshow, ylimits, yunits, yshow, ...
+function [] = format_axes(ax, xlim, xunits, xshow, ylim, yunits, yshow, ...
                 climits, cunits, title_str)
 
 axis equal
 grid on
 
-set(ax, 'XLim', xlimits, ...
-        'YLim', ylimits, ...
+set(ax, 'XLim', xlim, ...
+        'YLim', ylim, ...
         'YDir', 'normal', ...
         'Color', 0.9*[1, 1, 1]);
 
