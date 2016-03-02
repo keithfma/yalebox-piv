55c6c03b23c72eafc7e365041fcbedb0b8d42c5c
keithfma <keithfma@gmail.com>
2016-03-01 21:57:22 -0500

    plot_displacement: changed inputs, added data reading...
    Elected to use the netCDF, time-step index, and normalization bounding box.

--- plot_displacement.m
+++ plot_displacement.m
@@ -1,4 +1,4 @@
-function [] = plot_displacement(xx, yy, uu, vv, bbox)
+function [] = plot_displacement(piv_file, index, bbox)
 % function [] = plot_displacement(xx, yy, uu, vv, bbox)
 %
 % Plot normalized displacement magnitude and direction. Function generates 3 separate plots:
@@ -8,16 +8,42 @@ function [] = plot_displacement(xx, yy, uu, vv, bbox)
 % 
 % Arguments:
 %
-% xx, yy = Vectors, x- and y-direction coordinate vectors in [meters] with the
-%   origin at the s-point, corresponding to uu, vv
+% piv_file = String, file name of netCDF file containing output from the
+%   piv_series() analysis
 %
-% uu, vv = 2D matrix, x- and y- direction displacement vector components in
-%   [meters/step]
+% index = Scalar, index of timestep in piv_file to plot, uses MATLAB-style
+%   1-based indices.
 %
-% bbox = Vector, length==4, bounding box for the data region to be used to
-%       compute displacement normalization. Provided in world coordinates, e.g.
-%       [meters] with the origin at the s-point, formatted as 
-%       [left, bottom, width, height]
+% bbox = (optional) Vector, length==4, bounding box for the data region to be
+%   used to compute displacement normalization. Provided in world coordinates,
+%   formatted as [left, bottom, width, height]. *If empty, data are not
+%   normalized*
 % %
 
+% debug: hard-code input arguments
+piv_file = '~/Documents/dissertation/yalebox-exp-fault/data/fault_ss_01/piv/fault_ss_01_sidef.displ.nc';
+index = 100;
+bbox = [];
+
 % local constants
+font_size = 14;
+
+% normalize?
+normalize = true;
+if isempty(bbox)
+    normalize = false;
+end
+
+% check for sane inputs
+validateattributes(piv_file, {'char'}, {'vector'});
+validateattributes(index, {'numeric'}, {'scalar', 'positive', 'integer'});
+if normalize
+    validateattributes(bbox, {'numeric'}, {'vector', 'real', 'numel', 4});
+end
+
+% read data from netCDF
+xx = ncread(piv_file, 'x'); nx = numel(xx);
+yy = ncread(piv_file, 'y'); ny = numel(yy);
+step = ncread(piv_file, 'step', index, 1);
+uu = squeeze(ncread(piv_file, 'u', [1, 1, index], [nx, ny, 1]))';
+vv = squeeze(ncread(piv_file, 'v', [1, 1, index], [nx, ny, 1]))';
