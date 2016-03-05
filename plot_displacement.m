bb5ecad8b4cd96ff7c1c802a0056bd59499b1dfa
keithfma <keithfma@gmail.com>
2016-03-05 15:45:18 -0500

    plot_displacement: finalized quiver plot

--- plot_displacement.m
+++ plot_displacement.m
@@ -1,4 +1,5 @@
-function [] = plot_displacement(piv_file, index, xlim, ylim, ulim, vlim, mlim)
+function [] = plot_displacement(piv_file, index, xlim, ylim, ulim, vlim, mlim, ...
+                  qsize, qbnd, qscale)
 %
 % Plot displacement magnitude and direction. Function generates 3 separate plots:
 %   - x-direction displacement 
@@ -14,20 +15,30 @@ function [] = plot_displacement(piv_file, index, xlim, ylim, ulim, vlim, mlim)
 % index = Scalar, index of timestep in piv_file to plot, uses MATLAB-style
 %   1-based indices.
 %
-% xlim, ylim, ulim, vlim, mlim = Vector, length==2, [minimum, maximum] values for the x-axis, y-axis, u-displacement, v-displacement, and
+% xlim, ylim, ulim, vlim, mlim = (Optional) Vector, length==2, [minimum,
+%   maximum] values for the x-axis, y-axis, u-displacement, v-displacement, and
 %   displacement magnitude. Values beyond the range of the data will be
 %   truncated to the data limits. Thus, to span the data, one could use [-inf, inf].
+%
+% qsize = (Optional) Vector, length==2, size of the grid for vector direction
+%   overlay (quiver) in [rows, cols]. 
+%
+% qbnd = (Optional) Scalar, boundary margin for vector direction overlay, as
+%   fraction of the axis ranges.
+%
+% qscale = (Optional) Scalar, range [0,1], length of vector lines in vector
+%   direction overlay 
 % %
 
 % set defaults
-if nargin < 3 || isempty(xlim); xlim = [-inf, inf]; end
-if nargin < 4 || isempty(ylim); ylim = [-inf, inf]; end
-if nargin < 5 || isempty(ulim); ulim = [-inf, inf]; end
-if nargin < 6 || isempty(vlim); vlim = [-inf, inf]; end
-if nargin < 7 || isempty(mlim); mlim = [-inf, inf]; end
-
-% local parameters
-tension = 0.9;
+if nargin < 3  || isempty(xlim);   xlim = [-inf, inf]; end
+if nargin < 4  || isempty(ylim);   ylim = [-inf, inf]; end
+if nargin < 5  || isempty(ulim);   ulim = [-inf, inf]; end
+if nargin < 6  || isempty(vlim);   vlim = [-inf, inf]; end
+if nargin < 7  || isempty(mlim);   mlim = [-inf, inf]; end
+if nargin < 8  || isempty(qsize);  qsize = [20, 10];   end
+if nargin < 9  || isempty(qbnd);   qbnd = 0.05;        end
+if nargin < 10 || isempty(qscale); qscale = 0.1;       end
 
 % debug: hard-code input arguments
 piv_file = '~/Documents/dissertation/yalebox-exp-fault/data/fault_ss_01/piv/fault_ss_01_sidef.displ.nc';
@@ -37,7 +48,26 @@ ylim = [-inf, 0.12];
 ulim = [-inf, inf];
 vlim = [-inf, inf];
 mlim = [-inf, inf];
+qsize = [10, 20];
+qbnd = 0.05;
+qscale = 0.15;
 % end debug
+
+% check for sane inputs
+validateattributes(piv_file, {'char'}, {'vector'});
+assert(exist(piv_file, 'file') == 2);
+validateattributes(index, {'numeric'}, {'scalar', 'integer'});
+validateattributes(xlim, {'numeric'}, {'vector', 'numel', 2});
+validateattributes(ylim, {'numeric'}, {'vector', 'numel', 2});
+validateattributes(ulim, {'numeric'}, {'vector', 'numel', 2});
+validateattributes(vlim, {'numeric'}, {'vector', 'numel', 2});
+validateattributes(mlim, {'numeric'}, {'vector', 'numel', 2});
+validateattributes(qsize, {'numeric'}, {'vector', 'numel', 2, 'integer'});
+validateattributes(qbnd, {'numeric'}, {'scalar', '>=', 0, '<=', 1});
+validateattributes(qscale, {'numeric'}, {'scalar', '>=', 0, '<=', 1});
+
+% local parameters
+tension = 0.9;
 
 % check for sane inputs
 validateattributes(piv_file, {'char'}, {'vector'});
@@ -92,58 +122,51 @@ ulim = ulim*1000;
 vlim = vlim*1000;
 mlim = mlim*1000;
 
+% interpolate vectors to low-res checkerboard grid, normalize length for quiver plots
+%...generate staggered regular grid
+qx = linspace(xlim(1)+range(xlim)*qbnd, xlim(2)-range(xlim)*qbnd, qsize(2));
+qy = linspace(ylim(1)+range(ylim)*qbnd, ylim(2)-range(ylim)*qbnd, qsize(1));
+[qx, qy] = meshgrid(qx, qy);
+chkr = bsxfun(@xor, mod(1:qsize(2), 2), mod(1:qsize(1), 2)');
+qx = qx(chkr);
+qy = qy(chkr);
+%...crop grid to the roi
+[xgrid, ygrid] = meshgrid(xx, yy);
+roi = ~isnan(uu);
+roiq = interp2(xgrid, ygrid, roi, qx, qy, 'nearest');
+qx = qx(roiq);
+qy = qy(roiq);
+%...interpolate vectors
+qu = spline2d(qx, qy, xgrid(roi), ygrid(roi), uu(roi), tension);
+qv = spline2d(qx, qy, xgrid(roi), ygrid(roi), vv(roi), tension);
+%...normalize vector length
+mq = sqrt(qu.^2+qv.^2);
+qu = qu./mq;
+qv = qv./mq;
+
 % create figures and axes
 [ax_top, ax_mid, ax_bot] = create_figure();
 
 % plot displacement magnitude and direction
 axes(ax_top)
 imagesc(xx, yy, mm, 'AlphaData', ~isnan(mm)); 
+plot_direction(qx, qy, qu, qv, qscale);
 format_axes(gca, xlim, xunits, 0, ylim, yunits, 1, mlim, cunits, ...
     sprintf('displacement vector magnitude, step = %.1f', step));
 
 % plot x-direction displacement magnitude
 axes(ax_mid);
 imagesc(xx, yy, uu, 'AlphaData', ~isnan(uu)); 
+plot_direction(qx, qy, qu, qv, qscale);
 format_axes(gca, xlim, xunits, 0, ylim, yunits, 1, ulim, cunits, ...
     sprintf('x-direction displacement component, step = %.1f', step));
 
 % plot y-direction displacement magnitude
 axes(ax_bot);
 imagesc(xx, yy, vv, 'AlphaData', ~isnan(vv)); 
+plot_direction(qx, qy, qu, qv, qscale);
 format_axes(gca, xlim, xunits, 1, ylim, yunits, 1, vlim, cunits, ...
     sprintf('y-direction displacement component, step = %.1f', step));
-
-% debug: add quiver plot to top
-axes(ax_top)
-
-% quiver parameters
-q_bnd_frac = 0.05;
-nxq = 10; 
-nyq = 5; 
-
-% generate staggered regular grid
-xq = linspace(xlim(1)+range(xlim)*q_bnd_frac, xlim(2)-range(xlim)*q_bnd_frac, 2*nxq);
-yq = linspace(ylim(1)+range(ylim)*q_bnd_frac, ylim(2)-range(ylim)*q_bnd_frac, 2*nyq);
-[xq, yq] = meshgrid(xq, yq);
-checkerboard = bsxfun(@xor, mod(1:2*nxq, 2), mod(1:2*nyq, 2)');
-xq = xq(checkerboard);
-yq = yq(checkerboard);
-
-% crop grid to the roi
-[xgrid, ygrid] = meshgrid(xx, yy);
-roi = ~isnan(uu);
-roiq = interp2(xgrid, ygrid, roi, xq, yq, 'nearest');
-xq = xq(roiq);
-yq = yq(roiq);
-
-% interpolate vectors and plot
-uq = spline2d(xq, yq, xgrid(roi), ygrid(roi), uu(roi), tension);
-vq = spline2d(xq, yq, xgrid(roi), ygrid(roi), vv(roi), tension);
-
-hold on
-quiver(xq, yq, uq, vq);
-
-keyboard
 
 end
 
@@ -171,6 +194,12 @@ bot = axes('Position', bot_position);
 mid = axes('Position', mid_position);
 top = axes('Position', top_position);
 
+end
+
+function [] = plot_direction(qx, qy, qu, qv, qscale)
+    hold on
+    quiver(qx, qy, qu, qv, qscale, 'Color', 'w');
+    hold off
 end
 
 function [] = format_axes(ax, xlim, xunits, xshow, ylim, yunits, yshow, ...
