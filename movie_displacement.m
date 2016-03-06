586e32b609d58cc06175f35d3801a0ec8e0e709c
keithfma <keithfma@gmail.com>
2016-03-06 14:32:46 -0500

    movie_displacement: abandoned internal VideoWriter for image frames...
    New appraoch is to generate image frames at the desired resolution (FHD), then
    stich these together into a video using the full-featured avconv external
    program. The motivation is the difficulty of specifying 1) video format, and 2)
    video resolution. Specifically, MATLAB does not provide many options for video
    encoding on Linux, and video playback does not perform well for arbitrary
    resolutions. The solution is to use MATLAB to generate images (which is hard
    enough) and then create a video externally.

--- movie_displacement.m
+++ movie_displacement.m
@@ -1,4 +1,4 @@
-function [] = movie_displacement(piv_file, movie_file, clim, xlim, ylim, qsize, qbnd, qscale, show_frame)
+function [] = movie_displacement(piv_file, movie_file, clim, xlim, ylim, qsize, qbnd, qscale, cleanup, show_frame)
 % Generate movie from PIV displacement results. Calls plot_displacement_norm()
 % to create frames for each time step. 
 %
@@ -16,12 +16,17 @@ function [] = movie_displacement(piv_file, movie_file, clim, xlim, ylim, qsize, 
 %   qsize, qbnd, qscale = quiver plot parameters, see plot_displacement help for
 %       details
 %
+%   cleanup = OPTIONAL, delete temporary image files (1) or don't (0)
+%
 %   show_frame = OPTIONAL, show a single frame at step == show_frame, do not
 %       process other frames or make a movie, used for testing parameter values.
 % %
 
+% local constants
+tmp_dir = './tmp_movie_displacement';
+
 % parse inputs
-make_movie = nargin < 7 || isempty(show_frame);
+% make_movie = nargin < 9 || isempty(show_frame);
 
 % get color axis limits from vector quantiles
 uu = ncread(piv_file, 'u');
@@ -31,17 +36,39 @@ ulim = quantile(uu(:), clim);
 vlim = quantile(vv(:), clim);
 mlim = quantile(mm(:), clim);
 num_steps = size(uu, 3);
+
+% init loop
 clear uu vv mm
+bbox = [];
+mkdir(tmp_dir);
+
 
 % loop over all timesteps
-bbox = [];
-for ii = 1:num_steps
+for ii = 1:100; %num_steps
     
     % plot frame
     bbox = plot_displacement_norm(piv_file, ii, bbox, xlim, ylim, ulim, vlim, mlim, ...
         qsize, qbnd, qscale);
     
-    pause(1)
-    close(gcf);
+    % first time: get parameters needed to convert figure to FHD image (1920x1080)
+    if ii == 1
+        % get magnification factor from simple test image
+        img_size = size(export_fig('-dpng'));
+        img_size = img_size(1:2); % rows, cols
+        magnify = sprintf('-m%.10f', min([1080, 1920]./img_size));       
+        
+        % get padding from magnified test image
+        img_size = size(export_fig('-dpng', magnify));
+        img_size = img_size(1:2);
+        vpad = [floor((1080-img_size(1))/2), ceil((1080-img_size(1))/2)]; 
+        hpad = [floor((1920-img_size(2))/2), ceil((1920-img_size(2))/2)];         
+    end
+    
+    % convert figure to FHD image (1920x1080)
+    img = export_fig('-dpng', magnify);
+    img = padarray(img, [vpad(1), hpad(1), 0], 1, 'pre');
+    img = padarray(img, [vpad(2), hpad(2), 0], 1, 'post');
+    imwrite(img, sprintf('tmp_%04i.png', ii));
 
+    close(gcf);
 end
