55294ae5628425c7bc72cfc057ccb5b535e8cf60
keithfma <keithfma@gmail.com>
2016-03-05 22:49:16 -0500

    movie_displacement: changed input parameters for color axis

--- movie_displacement.m
+++ movie_displacement.m
@@ -1,39 +1,47 @@
-function [] = movie_displacement(piv_file, movie_file, param, show_frame)
+function [] = movie_displacement(piv_file, movie_file, clim, xlim, ylim, qsize, qbnd, qscale, show_frame)
+% Generate movie from PIV displacement results. Calls plot_displacement_norm()
+% to create frames for each time step. 
 %
-% piv_file = String, path to netCDF containing PIV results produced by piv.m
+% Arguments:
 %
-% movie_file = String path to output video file, without file extension
+%   piv_file = String, path to netCDF containing PIV results produced by piv.m
 %
-% param = Struct, movie parameters, as produced by movie_displacement_default.m
-%   .xlim   -|
-%   .ylim    |
-%   .ulim    |
-%   .vlim    |
-%   .mlim    |-> See plot_displacement help for details
-%   .qsize   |
-%   .qbnd    |
-%   .qscale -|
-%   .frame_rate = Video frame rate in FPS
-%   .max_dim = Maximum video dimensionsions in pixels
+%   movie_file = String path to output video file, without file extension
 %
-% show_frame = OPTIONAL, show a single frame at step == show_frame, do not
-%   process other frames or make a movie, used for testing parameter
-%   values.
+%   clim =  Vector, length ==2, color axis limits as quantiles of the whole data
+%       set. Sets ulim, vlim, mlim parameters to plot_displacement
+%   
+%   xlim, ylim = 
+%
+%   qsize, qbnd, qscale = quiver plot parameters, see plot_displacement help for
+%       details
+%
+%   show_frame = OPTIONAL, show a single frame at step == show_frame, do not
+%       process other frames or make a movie, used for testing parameter values.
 % %
 
 % parse inputs
-make_movie = nargin < 4 || isempty(show_frame);
+make_movie = nargin < 7 || isempty(show_frame);
 
-% initialize loopsave
-step_info = ncinfo(piv_file, 'step');
-num_step = step_info.Size;
+% get color axis limits from vector quantiles
+uu = ncread(piv_file, 'u');
+vv = ncread(piv_file, 'v');
+mm = sqrt(uu.^2 + vv.^2);
+ulim = quantile(uu(:), clim);
+vlim = quantile(vv(:), clim);
+mlim = quantile(mm(:), clim);
+num_steps = size(uu, 3);
+clear uu vv mm
 
-for ii = 1:num_step
+% loop over all timesteps
+bbox = [];
+for ii = 1:num_steps
     
     % plot frame
-    plot_displacement_norm(piv_file, ii, param.bbox, param.xlim, param.ylim, ...
-        param.ulim, param.vlim, param.mlim, param.qsize, param.qbnd, param.qscale);
+    bbox = plot_displacement_norm(piv_file, ii, bbox, xlim, ylim, ulim, vlim, mlim, ...
+        qsize, qbnd, qscale);
     
-    pause
+    pause(1)
+    close(gcf);
 
 end
