097fcebc21a91cf3d975af71ea3d13d4c51a3601
keithfma <keithfma@gmail.com>
2016-03-07 00:25:28 -0500

    Revised movie_displ complete through single frame plot

--- movie_displ.m
+++ movie_displ.m
@@ -1,4 +1,4 @@
-function opt = movie_displ(piv_file, movie_file, show_frame, varargin)
+function opt = movie_displ(piv_file, movie_file, varargin)
 % Generate movie from PIV displacement results. 
 %
 % Arguments:
@@ -7,17 +7,14 @@ function opt = movie_displ(piv_file, movie_file, show_frame, varargin)
 %
 %   movie_file = String path to output video file, without file extension
 %
-%   show_frame = OPTIONAL, show a single frame at index == show_frame, do not
+% Parameters:
+%
+%   'show_frame' = show a single frame at index == show_frame, do not
 %       process other frames or make a movie. Used for testing parameter values.
 %
-% Parameters:
 %   'coord_units' = String, name of units for coordinate axes, coordinate values
 %       will be rescaled accordingly, and labels will reflect these units. Valid
 %       options are: 'm', 'cm'. Default = 'cm'
-%
-%   'displ_units' = String, name of units for displacement vector components and
-%       magnitude, values will be rescaled accordingly, and labels will reflect
-%       these units. Valid options are: 'm', 'mm', '1'. Default is 'mm'.
 %
 %   'norm_bbox' = Vector, length==4, bounding box for the data region used in
 %       computing the displacement unit normalization factor. This is only used
@@ -53,10 +50,10 @@ assert(exist(movie_file, 'file') ~= 2); % do not overwrite
 % parameter name-value pairs
 ip = inputParser();
 
+ip.addParameter('show_frame', 0, ...
+    @(x) validateattributes(x, {'numeric'}, {'scalar', 'integer'}));
 ip.addParameter('coord_units', 'cm', ...
     @(x) ismember(x, {'m', 'cm'})); 
-ip.addParameter('displ_units', 'mm/step', ...
-    @(x) ismember(x, {'m/step', 'mm/step', '1'}));
 ip.addParameter('norm_bbox', [], ...
     @(x) validateattributes(x, {'numeric'}, {'vector', 'numel', 4}));
 ip.addParameter('xlim', [-inf, inf], ...
@@ -75,4 +72,57 @@ ip.addParameter('qscale', 0.2, ...
 ip.parse(varargin{:});
 opt = ip.Results;
 
+%% get color axis limits from global quantiles of normalized data
+
+% read all data to memory 
+xx = ncread(piv_file, 'x'); 
+yy = ncread(piv_file, 'y'); 
+uu = permute( ncread(piv_file, 'u'), [2, 1, 3] );
+vv = permute( ncread(piv_file, 'v'), [2, 1, 3] );
+mm = sqrt(uu.^2 + vv.^2);
+
+% normalize each step
+num_steps = size(uu, 3);
+for ii = 1:num_steps
+    [uu(:,:,ii), vv(:,:,ii), mm(:,:,ii), opt.norm_bbox] = ...
+        util_normalize_displ(xx, yy, uu(:,:,ii), vv(:,:,ii), mm(:,:,ii), opt.norm_bbox);     
+end
+
+% compute limits from quantiles
+ulim = quantile(uu(:), opt.clim);
+vlim = quantile(vv(:), opt.clim);
+mlim = quantile(mm(:), opt.clim);
+
+% convert coordinate units
+switch opt.coord_units
+    case 'm'
+        % do nothing
+    case 'cm'
+        opt.norm_bbox = opt.norm_bbox*100;
+end
+
+% clean up section
+clear uu vv mm
+
+%% test: single-frame only
+
+if opt.show_frame ~= 0
+    
+    plot_displ(piv_file, opt.show_frame, ...
+        'coord_units', opt.coord_units, ...
+        'displ_units', '1', ...
+        'norm_bbox', opt.norm_bbox, ...
+        'xlim', opt.xlim, ...
+        'ylim', opt.ylim, ...
+        'ulim', ulim, ...
+        'vlim', vlim, ...
+        'mlim', mlim, ...
+        'qsize', opt.qsize, ...
+        'qbnd', opt.qbnd, ...
+        'qscale', opt.qscale);
+    
+end
+
+
 keyboard
+    
