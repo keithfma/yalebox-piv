4e5dbb42ace89c3e387e01992cf0005a5a530eeb
keithfma <keithfma@gmail.com>
2016-03-09 22:15:53 -0500

    Added help message and argument validation

--- piv_peak_interp.m
+++ piv_peak_interp.m
@@ -1,13 +1,22 @@
 function [rpk, cpk, val] = piv_peak_interp(zz, precision)
 %
-% Locate subpixel peak with specified precision by brute-force interpolation.
+% Locate subpixel peak with specified precision by upscaling in the vicinity of
+% the peak. Uses MATLAB's built in interpolation routines for simplicity.
+%
+% Arguments:
+%
+%   zz = 2D matrix, data set for which to estimate peak
+%
+%   precision = Scalar, grid spacing in fine (high-res) grid, which gives the
+%       precision of the subplixel peak location estimate, must divide evenly
+%       into 1 (e.g 0.1 is OK, 0.3 is not) so that the fine grid is aligned with
+%       the coarse.
 % %
 
-% debug: hard-coded inputs for testing
-zz = peaks(100);
-precision = 0.001;
-
-% assume sane inputs for performance
+% check for sane inputs
+validateattributes(zz, {'numeric'}, {'2d', 'real'});
+validateattributes(precision, {'numeric'}, {'scalar', '<', 1});
+assert(mod(1/precision, 1) == 0);
 
 % get peak, fail if no unique maximum 
 [rpk, cpk] = find(zz == max(zz(:)));
@@ -34,9 +43,7 @@ rpk = row_fine(ipk);
 
 end
 
-
-
-function [rr, cc, vv, ss] = fail()
+function [rr, cc, vv] = fail()
 % Set output values and throw a warning if the main function fails to find the
 % subpixel peak.
 
