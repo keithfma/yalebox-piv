d2d33f2e49719993f2c62b27c21c186b398dc596
Keith Ma <keithma@bu.edu>
2016-03-15 00:06:24 -0400

    Updated batch script template (untested)

--- util_batch_piv.scc.sh
+++ util_batch_piv.scc.sh
@@ -9,11 +9,11 @@
 
 # define parameters
 yalebox_path=/projectnb/glaciermod/yalebox-piv
-data_top_dir=/projectnb/glaciermod/yalebox-exp-fault/data/fault_01_ss
+data_top_dir=/projectnb/glaciermod/yalebox-exp-fault/data/fault_ss_01/piv
 data_run_name=fault_ss_01_sidef
-param_file=$data_top_dir/piv/$data_run_name.piv_param.mat
-input_file=$data_top_dir/piv/$data_run_name.input.nc
-output_file=$data_top_dir/piv/$data_run_name.displ.nc
+param_file=$data_top_dir/$data_run_name.piv_param.mat
+input_file=$data_top_dir/$data_run_name.input.nc
+output_file=$data_top_dir/$data_run_name.displ.nc
 
 # setup runtime environment
 module load matlab/2015b
@@ -22,6 +22,8 @@ export MATLABPATH=$yalebox_path:$MATLABPATH
 # run
 matlab -nodisplay << EOF
 load $param_file
-piv_series('$output_file', '$input_file', samplen, sampspc, intrlen, npass, valid_max, valid_eps, lowess_span_pts, spline_tension, min_frac_data, min_frac_overlap, low_res_spc, verbose)
+whos
+util_setup_env();
+piv_series('$output_file', '$input_file', samplen, sampspc, intrlen, npass, valid_max, valid_eps, lowess_span_pts, spline_tension, min_frac_data, min_frac_overlap, 1);
 exit
 EOF
