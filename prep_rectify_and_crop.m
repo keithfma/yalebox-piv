9e093046d974801588a2f60d8c47debf7e1a1c0b
Keith Ma <keithma@bu.edu>
2016-05-02 11:33:54 -0400

    Mostly working rectification routine...
    Input arguments need to be standardized, and coordinate vectors are not
    yet generated.

--- prep_rectify_and_crop.m
+++ prep_rectify_and_crop.m
@@ -1,11 +1,14 @@
-%function [] = prep_rectify_and_crop(woco)
+%function [] = prep_rectify_and_crop(woco, img, )
 %
 % Use manually defined world-coordinate control points to project image into a
 % regular coordinate system, and crop this image to the desired size.
 
 load k24_woco_side.mat
-woco_world_dx = 0.1; % used to find neighbors
-woco_world_dy = 0.1;
+im = imread('woco.jpg');
+origin_xw = -0.6;
+origin_yw = 0.0;
+crop_xw = [-0.6, 0.7];
+crop_yw = [0.0, 0.3];
 
 %  breakup woco struct
 ctrl_xp = woco.xp;
@@ -20,3 +23,19 @@ Dw = pdist([ctrl_xw, ctrl_yw]);
 Dp = pdist([ctrl_xp, ctrl_yp]);
 delta_pixel_per_meter = median(Dp./Dw);
 delta_meter_per_pixel = median(Dw./Dp);
+
+% get rectified pixel coordinates from origin and pixel size
+ctrl_xr = 1+(ctrl_xw-origin_xw)*delta_pixel_per_meter;
+ctrl_yr = 1+(ctrl_yw-origin_yw)*delta_pixel_per_meter;
+ 
+% transform image to rectified coordinates
+tform = fitgeotrans([ctrl_xp, ctrl_yp], [ctrl_xr, ctrl_yr],'lwm', 10);
+warning('off', 'images:inv_lwm:cannotEvaluateTransfAtSomeOutputLocations');
+imr = imwarp(im, tform);
+warning('on', 'images:inv_lwm:cannotEvaluateTransfAtSomeOutputLocations');
+
+% crop rectified image
+crop_xr = 1+(crop_xw-origin_xw)*delta_pixel_per_meter;
+crop_yr = 1+(crop_yw-origin_yw)*delta_pixel_per_meter;
+cropbox = [crop_xr(1), crop_yr(1), diff(crop_xr), diff(crop_yr)];
+imrc = imcrop(imr, cropbox);
