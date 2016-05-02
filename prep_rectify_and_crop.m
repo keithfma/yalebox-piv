92cc930e877d30dd7df7e38453be2a490a963df0
keithfma <keithfma@gmail.com>
2016-05-02 07:40:53 -0400

    Skeleton for rectify and crop routine

---
+++ prep_rectify_and_crop.m
@@ -0,0 +1,4 @@
+function [] = prep_rectify_and_crop()
+%
+% Use manually defined world-coordinate control points to project image into a
+% regular coordinate system, and crop this image to the desired size.
