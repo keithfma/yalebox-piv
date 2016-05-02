51ddcad8f4c61ebd53af6a74d0e2416974186cc2
Keith Ma <keithma@bu.edu>
2016-05-02 12:11:41 -0400

    Complete, working rectify and crop routine

--- prep_rectify_and_crop.m
+++ prep_rectify_and_crop.m
@@ -1,14 +1,12 @@
-%function [] = prep_rectify_and_crop(woco, img, )
+%function [rgb_r, coord_x, coord_y] = prep_rectify_and_crop(woco, rgb, crop_xlim_world, crop_ylim_world)
 %
 % Use manually defined world-coordinate control points to project image into a
 % regular coordinate system, and crop this image to the desired size.
 
 load k24_woco_side.mat
-im = imread('woco.jpg');
-origin_xw = -0.6;
-origin_yw = 0.0;
-crop_xw = [-0.6, 0.7];
-crop_yw = [0.0, 0.3];
+rgb = imread('woco.jpg');
+crop_xlim_world = [-0.6, 0.6];
+crop_ylim_world = [0.0, 0.25];
 
 %  breakup woco struct
 ctrl_xp = woco.xp;
@@ -25,17 +23,23 @@ delta_pixel_per_meter = median(Dp./Dw);
 delta_meter_per_pixel = median(Dw./Dp);
 
 % get rectified pixel coordinates from origin and pixel size
-ctrl_xr = 1+(ctrl_xw-origin_xw)*delta_pixel_per_meter;
-ctrl_yr = 1+(ctrl_yw-origin_yw)*delta_pixel_per_meter;
+ctrl_xr = 1+(ctrl_xw-crop_xlim_world(1))*delta_pixel_per_meter;
+ctrl_yr = 1+(ctrl_yw-crop_ylim_world(1))*delta_pixel_per_meter;
  
 % transform image to rectified coordinates
-tform = fitgeotrans([ctrl_xp, ctrl_yp], [ctrl_xr, ctrl_yr],'lwm', 10);
 warning('off', 'images:inv_lwm:cannotEvaluateTransfAtSomeOutputLocations');
-imr = imwarp(im, tform);
+tform = fitgeotrans([ctrl_xp, ctrl_yp], [ctrl_xr, ctrl_yr],'lwm', 10);
+rgb_r = imwarp(rgb, tform);
 warning('on', 'images:inv_lwm:cannotEvaluateTransfAtSomeOutputLocations');
 
 % crop rectified image
-crop_xr = 1+(crop_xw-origin_xw)*delta_pixel_per_meter;
-crop_yr = 1+(crop_yw-origin_yw)*delta_pixel_per_meter;
-cropbox = [crop_xr(1), crop_yr(1), diff(crop_xr), diff(crop_yr)];
-imrc = imcrop(imr, cropbox);
+crop_xlim_r = 1+(crop_xlim_world-crop_xlim_world(1))*delta_pixel_per_meter;
+crop_ylim_r = 1+(crop_ylim_world-crop_ylim_world(1))*delta_pixel_per_meter;
+cropbox = [crop_xlim_r(1), crop_ylim_r(1), diff(crop_xlim_r), diff(crop_ylim_r)];
+rgb_r = imcrop(rgb_r, cropbox);
+
+% generate coordinate vectors (assumes that x=0, y = 0 is a control point)
+origin_xr = ctrl_xr(ctrl_xw == 0  & ctrl_yw == 0);
+origin_yr = ctrl_yr(ctrl_xw == 0  & ctrl_yw == 0);
+coord_x = ((1:size(rgb_r, 2))-origin_xr)*delta_meter_per_pixel;
+coord_y = ((1:size(rgb_r, 1))-origin_yr)*delta_meter_per_pixel;
