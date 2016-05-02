8c1e794be2a16afc74fab8f09fc7a8d9c1505e37
Keith Ma <keithma@bu.edu>
2016-05-02 10:56:11 -0400

    Selected all points method, removed obsolete code

--- prep_rectify_and_crop.m
+++ prep_rectify_and_crop.m
@@ -15,36 +15,8 @@ ctrl_yw = woco.yw;
 num_ctrl = numel(ctrl_xp);
 ctrl_index = (1:num_ctrl)';
 
-% option 1: get pixel size from pairwise distance between neighboring grid points
-dist_world = [];
-dist_pixel = [];
-for ii = 1:num_ctrl
-    
-    nbrs = (abs(ctrl_xw(ii)-ctrl_xw) < (1.1*woco_world_dx)) & ...
-        (abs(ctrl_yw(ii)-ctrl_yw) < (1.1*woco_world_dy)) & ...
-        (ctrl_index ~= ii);
-    
-    nbrs_dist_world = sqrt((ctrl_xw(ii)-ctrl_xw(nbrs)).^2 + ...
-        (ctrl_yw(ii)-ctrl_yw(nbrs)).^2);
-    
-    dist_world = [dist_world; nbrs_dist_world(:)]; %#ok!
-    
-    nbrs_dist_pixel = sqrt((ctrl_xp(ii)-ctrl_xp(nbrs)).^2 + ...
-        (ctrl_yp(ii)-ctrl_yp(nbrs)).^2);
-    
-    dist_pixel = [dist_pixel; nbrs_dist_pixel(:)]; %#ok!
-end
-
-figure
-hist(dist_world./dist_pixel*1000, 25);
-title('neighboring points only');
-fprintf('neighboring points only: delta = %.4f mm/pixel\n', median(dist_world./dist_pixel*1000));
-
-% option 2: get pixel size from pairwise distance between all points
+% get pixel size from median of pairwise distance between all points
 Dw = pdist([ctrl_xw, ctrl_yw]);
 Dp = pdist([ctrl_xp, ctrl_yp]);
-
-figure
-hist(Dw./Dp*1000, 25);
-title('all points');
-fprintf('all points: delta = %.4f mm/pixel\n', median(Dw./Dp*1000));
+delta_pixel_per_meter = median(Dp./Dw);
+delta_meter_per_pixel = median(Dw./Dp);
