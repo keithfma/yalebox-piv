dde8bbe8cc54328fb3ab2c4ce934cc01a14f4d69
keithfma <keithfma@gmail.com>
2016-05-10 21:32:04 -0400

    Complete update of prep_get_param for reverted auto masking

--- prep_get_param.m
+++ prep_get_param.m
@@ -5,9 +5,17 @@
 
 %% Init
 
+% parameters
 woco_image_file = 'test/prep_get_parameters_woco.jpg';
 step_image_file = 'test/prep_get_parameters_step.jpg';
 param_out_file = 'test/prep_get_parameters.mat';
+output_file = 'junk.image.nc';
+image_path = '/home/kfm/Documents/dissertation/yalebox-exp-erosion/data/k24/image/clean';
+image_name_glob = 'K24*.jpg';
+
+% actions
+tmp = dir(fullfile(image_path, image_name_glob));
+image_names = {tmp(:).name};
 
 %% Define coordinate system control points
 
@@ -35,6 +43,7 @@ mask_manual = prep_mask_manual(rgb);
 
 %% Apply automatic masking model
 
+% parameters
 hue_lim = [0.01, 0.3];
 val_lim = [0.05, 0.5];
 entropy_lim = [0.4, 1];
@@ -42,6 +51,7 @@ entropy_len = 11;
 morph_open_rad = 10;
 morph_erode_rad = 5;
 
+% actions
 mask_auto = prep_mask_auto(rgb, hue_lim, val_lim, entropy_lim, entropy_len, ...
                     morph_open_rad, morph_erode_rad, true, true);
 
@@ -53,17 +63,9 @@ eql_len = 21;
 % actions
 eql = prep_intensity(rgb, mask_manual & mask_auto, eql_len, true, true);
 
-%% Get image file directory and file list
-
-output_file = 'junk.image.nc';
-image_path = '/home/kfm/Documents/dissertation/yalebox-exp-erosion/data/k24/image/clean';
-image_name_glob = 'K24*.jpg';
-
-tmp = dir(fullfile(image_path, image_name_glob));
-image_names = {tmp(:).name};
-
 %% Save parameters for batch processing
 
 save(param_out_file, 'ctrl_xw', 'ctrl_yw', 'ctrl_xp', 'ctrl_yp', 'crop_xw', ...
-    'crop_yw', 'entropy_len', 'num_cluster', 'cluster_center', 'eql_len', ...
-    'xw', 'yw', 'mask_manual', 'output_file', 'image_path', 'image_names'); 
+    'crop_yw', 'hue_lim', 'val_lim', 'entropy_lim', 'entropy_len', ...
+    'morph_open_rad', 'morph_erode_rad', 'eql_len', 'xw', 'yw', ...
+    'mask_manual', 'output_file', 'image_path', 'image_names'); 
