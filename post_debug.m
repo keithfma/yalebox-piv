53fd4bd1c65b27946df330b3e9e16e8402ae8a7d
Keith Ma <keithfma@gmail.com>
2016-10-18 00:26:12 -0400

    Updated test case to generate properly named file

--- post_debug.m
+++ post_debug.m
@@ -1,9 +1,10 @@
 % Script. Scratch space for debugging the post-processing code suite
 
 % parameters
+exp_name = 'fault_ss_01_sidef';
 data_dir = 'C:\Users\keithfma\home\dissertation\yalebox-exp-fault\data';
-piv_netcdf = fullfile(data_dir, 'fault_ss_01_sidef_piv.nc');
-post_netcdf = 'junk.nc';
+piv_netcdf = fullfile(data_dir, sprintf('%s_piv.nc', exp_name));
+post_netcdf = fullfile(data_dir, sprintf('%s_post.nc', exp_name));
 pro_bbox = [0.56, 0.0, 0.05, 0.1]; % limited in x, whole section in y
 retro_bbox = [-0.5, 0.0, 0.1, 0.1];
 
