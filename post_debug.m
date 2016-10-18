6ee94f0269f956efc784b9e6498b15a280e5659b
Keith Ma <keithfma@gmail.com>
2016-10-17 23:18:14 -0400

    Implemented displacement median in rect

--- post_debug.m
+++ post_debug.m
@@ -2,11 +2,13 @@
 
 % parameters
 data_dir = 'C:\Users\keithfma\home\dissertation\yalebox-exp-fault\data';
-piv_netcdf = fullfile(data_dir, 'fault_ss_01_siden_piv.nc');
+piv_netcdf = fullfile(data_dir, 'fault_ss_01_sidef_piv.nc');
 post_netcdf = 'junk.nc';
+pro_bbox = [0.56, 0.0, 0.05, 0.1]; % limited in x, whole section in y
+retro_bbox = [-0.5, 0.0, 0.1, 0.1];
 
 % cleanup previous run
 delete(post_netcdf);
 
 % try series post-processing 
-post_series(piv_netcdf, post_netcdf);
+post_series(piv_netcdf, post_netcdf, pro_bbox, retro_bbox);
--- post_series.m
+++ post_series.m
@@ -1,4 +1,4 @@
-function [] = post_series(piv_netcdf, post_netcdf)
+function [] = post_series(piv_netcdf, post_netcdf, pro_bbox, retro_bbox)
 % 
 % Run post-processing analyses on PIV data, and saves the results and
 % metadata in a netCDF file.
@@ -9,35 +9,63 @@ function [] = post_series(piv_netcdf, post_netcdf)
 %   post_netcdf: string, file name of netCDF to create for post-processing
 %       results data and metadata. Overwriting is not allowed to prevent
 %       unhappy accidents
+%   pro_bbox, retro_bbox: 4-element position vectors [xmin, ymin, width,
+%       height] for the bounding boxes used to estimate pro- and retro-
+%       plate displacements from PIV data. 
 %
 % Output netCDF is self-documenting
 %
 % % Keith Ma
 
 % parse input arguments
-validateattributes(piv_netcdf, {'char'}, {'nonempty'}, mfilename, 'piv_netcdf');
+validateattributes(piv_netcdf, {'char'}, {'nonempty'}, ...
+    mfilename, 'piv_netcdf');
 assert(exist(piv_netcdf, 'file') == 2, ...
     sprintf('input file %s does not exist', piv_netcdf));
-
-validateattributes(post_netcdf, {'char'}, {'nonempty'}, mfilename, 'post_netcdf');
+validateattributes(post_netcdf, {'char'}, {'nonempty'}, ...
+    mfilename, 'post_netcdf');
 assert(exist(post_netcdf, 'file') ~= 2, ...
     sprintf('output file %s already exists', piv_netcdf));    
+validateattributes(pro_bbox, {'numeric'}, {'vector', 'numel', 4}, ...
+    mfilename, 'pro_bbox');
+validateattributes(retro_bbox, {'numeric'}, {'vector', 'numel', 4}, ...
+    mfilename, 'retro_bbox');
 
 % read in PIV data from netCDF
+%...stored
 xx   = double(ncread(piv_netcdf, 'x')); 
 yy   = double(ncread(piv_netcdf, 'y')); 
 step = double(ncread(piv_netcdf, 'step'));
 uu   = double(ncread(piv_netcdf, 'u'));
 vv   = double(ncread(piv_netcdf, 'v'));
 roi  = double(ncread(piv_netcdf, 'roi'));
+%...derived
+mm = sqrt(uu.^2+vv.^2);
+num_steps = length(step);
 
 % create output file
 
 % run analyses for each timestep
-% % pro- and retro-plate velocity
-% % area budget
-% % infinitesimal strain analysis
-% % finite strain analysis
+uv_pro   = nan(num_steps, 2);
+uv_retro = nan(num_steps, 2);
+
+for ii = 1:num_steps
+    us = uu(:,:,ii);
+    vs = vv(:,:,ii);
+    uv_pro(ii,:)   = post_displ_rect(xx, yy, us, vs, pro_bbox);
+    uv_retro(ii,:) = post_displ_rect(xx, yy, us, vs, retro_bbox);
+    
+    % <DEBUG>
+    fprintf('step %d: uv_pro: [%.4f, %.4f] m/step\n', ...
+        ii, uv_pro(ii,:)*1000);
+    fprintf('step %d: uv_retro: [%.4f, %.4f] m/step\n', ...
+        ii, uv_retro(ii,:)*1000);
+    %</DEBUG>
+    
+    % area budget
+    % infinitesimal strain analysis
+    % finite strain analysis
+end
 
 % finalize
 
