41c7a3b39090df3e8caaf8fdc2553d99e7f5736c
Keith Ma <keithfma@gmail.com>
2016-11-24 09:19:02 -0500

    Updated MD5 hash to use native routine from FileExchange

--- util_md5_hash.m
+++ util_md5_hash.m
@@ -1,8 +1,8 @@
 function hash = util_md5_hash(file)
 % function hash = util_md5_hash(file)
 %
-% Compute MD5 hash of the input file, otherwise fail with an error. Currently
-% uses a system call to an external system tool (md5sum, linux).
+% Compute MD5 hash of the input file, otherwise fail with an error. Simple
+% wrapper around the FileExchange submission DataHash by Jan Simon.
 %
 % Arguments:
 %
@@ -10,17 +10,6 @@ function hash = util_md5_hash(file)
 %   hash = String, MD5 hash of input file
 % %
 
-if isunix
-    cmd = sprintf('md5sum %s', file);
-elseif ispc
-    cmd = sprintf('FCIV -md5 -add %s', file); % FAILS
-else
-    error('Operating system not supported');
-end
-
-[stat, out] = system(cmd);
-assert(stat == 0, 'Failed to compute MD5 hash');
-
-out_parts = strsplit(out, ' ');
-hash = out_parts{1};
-assert(length(hash) == 32, 'Failed to extract value of MD5 hash from stdout');
+opt = struct('Method', 'MD5', 'Format', 'hex', 'Input', 'file');
+hash = DataHash(file, opt);
+assert(length(hash) == 32, 'Failed to get MD5 hash');
