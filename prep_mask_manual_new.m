38948702a5656832ee5fab99819f974fe2935896
Keith Ma <keithfma@gmail.com>
2017-03-14 21:39:47 -0400

    Created new function for manual mask GUI

---
+++ prep_mask_manual_new.m
@@ -0,0 +1,10 @@
+function mask = prep_mask_manual_new(img)
+% function mask = prep_mask_manual(img)
+%
+% Interactive GUI to define a mask to black out region(s) of an image.
+%
+% Arguments:
+%   img = Matrix, image data that can be displayed by imshow
+%   mask = 2D matrix, logical, true where there is sand and false elsewhere.
+%
+% % Keith Ma
