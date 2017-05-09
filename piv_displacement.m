0f72146a12a6449f9504409b7d2a51541d7a54d1
Keith Ma <keithma@bu.edu>
2017-05-09 07:14:56 -0400

    Using MATLAB parallel computing toolbox (parfor) in piv_displacement

--- piv_displacement.m
+++ piv_displacement.m
@@ -36,73 +36,69 @@ min_overlap = min_frac_overlap*samplen*samplen; % frac to pixels
 
 % allocate outputs, skipped points remain NaN, false
 [nr, nc] = size(r_tm_i);
+nn = numel(r_tm_i);
 u_tm = nan(nr, nc);
 v_tm = nan(nr, nc);
 r_tm = nan(nr, nc);
 c_tm = nan(nr, nc);
 
-for ii = 1:nr
-    for jj = 1:nc
-   
-        % TODO: should return the center        
+parfor kk = 1:nn
         
-        % get sample window, offset to initial time
-        % NOTE: size(samp) may *not* be [samplen, samplen] due to rounding, it
-        %   follows that the true center point may not be r_ti, c_ti, that's OK
-        r_ti = r_tm_i(ii, jj) - 0.5*v_tm_i(ii, jj);
-        c_ti = c_tm_i(ii, jj) - 0.5*u_tm_i(ii, jj);
-        [samp, r_samp, c_samp] = piv_window(ini, r_ti, c_ti, samplen); 
-        
-        % skip if sample window is too empty
-        frac_data = sum(samp(:) ~= 0)/numel(samp);
-        if  frac_data < min_frac_data; 
-            continue; 
-        end        
-        
-        % get interrogation window, offset to final time
-        % NOTE: size(intr) may *not* be [intrlen, intrlen] due to rounding, it
-        %   follows that the true center point may not be r_ti, c_ti, that's OK
-        r_tf = r_tm_i(ii, jj) + 0.5*v_tm_i(ii, jj);
-        c_tf = c_tm_i(ii, jj) + 0.5*u_tm_i(ii, jj);
-        [intr, r_intr, c_intr] = piv_window(fin, r_tf, c_tf, intrlen);
-        
-        % compute masked, normalized cross correlation
-        [xcr, overlap] = normxcorr2_masked(intr, samp, intr~=0, samp~=0);
-        
-        % skip if nowhere has enough overlapping sandy pixels
-        if max(overlap(:)) < min_overlap
-            continue
-        end
-        
-        % crop correlation plane where not enough overlapping sandy pixels
-        xcr(overlap < min_overlap) = 0;
-        
-        % find peak with subpixel precision
-        % NOTE: two options below have approx the same accuracy in aggregate,
-        %   the "low-quality" option is faster, but has a multimodal error
-        %   distribution, the "high-quality" option is slower, but yields a
-        %   approx normal error distribution.
-        if high_quality
-            [r_peak, c_peak] = piv_peak_optim_interp(xcr, 1e-6);
-        else
-            [r_peak, c_peak] = piv_peak_gauss2d(xcr);
-        end
-        
-        % compute displacement
-        u_tm(ii, jj) = c_peak - size(samp, 2) - (c_samp(1) - c_intr(1));
-        v_tm(ii, jj) = r_peak - size(samp, 1) - (r_samp(1) - r_intr(1));
-
-        % get centroid of the sample window
-        [r_idx, c_idx] = find(samp ~= 0);
-        num = length(r_idx);
-        r_samp_cntr = sum(r_samp(r_idx))/num;
-        c_samp_cntr = sum(c_samp(c_idx))/num;
-        
-        % update observation point to midpoint time
-        r_tm(ii, jj) = r_samp_cntr + 0.5*v_tm(ii, jj);
-        c_tm(ii, jj) = c_samp_cntr + 0.5*u_tm(ii, jj);
-        
+    % get sample window, offset to initial time
+    % NOTE: size(samp) may *not* be [samplen, samplen] due to rounding, it
+    %   follows that the true center point may not be r_ti, c_ti, that's OK
+    r_ti = r_tm_i(kk) - 0.5*v_tm_i(kk);
+    c_ti = c_tm_i(kk) - 0.5*u_tm_i(kk);
+    [samp, r_samp, c_samp] = piv_window(ini, r_ti, c_ti, samplen);
+    
+    % skip if sample window is too empty
+    frac_data = sum(samp(:) ~= 0)/numel(samp);
+    if  frac_data < min_frac_data;
+        continue;
     end
+    
+    % get interrogation window, offset to final time
+    % NOTE: size(intr) may *not* be [intrlen, intrlen] due to rounding, it
+    %   follows that the true center point may not be r_ti, c_ti, that's OK
+    r_tf = r_tm_i(kk) + 0.5*v_tm_i(kk);
+    c_tf = c_tm_i(kk) + 0.5*u_tm_i(kk);
+    [intr, r_intr, c_intr] = piv_window(fin, r_tf, c_tf, intrlen);
+    
+    % compute masked, normalized cross correlation
+    [xcr, overlap] = normxcorr2_masked(intr, samp, intr~=0, samp~=0);
+    
+    % skip if nowhere has enough overlapping sandy pixels
+    if max(overlap(:)) < min_overlap
+        continue
+    end
+    
+    % crop correlation plane where not enough overlapping sandy pixels
+    xcr(overlap < min_overlap) = 0;
+    
+    % find peak with subpixel precision
+    % NOTE: two options below have approx the same accuracy in aggregate,
+    %   the "low-quality" option is faster, but has a multimodal error
+    %   distribution, the "high-quality" option is slower, but yields a
+    %   approx normal error distribution.
+    if high_quality
+        [r_peak, c_peak] = piv_peak_optim_interp(xcr, 1e-6);
+    else
+        [r_peak, c_peak] = piv_peak_gauss2d(xcr);
+    end
+    
+    % compute displacement
+    u_tm(kk) = c_peak - size(samp, 2) - (c_samp(1) - c_intr(1));
+    v_tm(kk) = r_peak - size(samp, 1) - (r_samp(1) - r_intr(1));
+    
+    % get centroid of the sample window
+    [r_idx, c_idx] = find(samp ~= 0);
+    num = length(r_idx);
+    r_samp_cntr = sum(r_samp(r_idx))/num;
+    c_samp_cntr = sum(c_samp(c_idx))/num;
+    
+    % update observation point to midpoint time
+    r_tm(kk) = r_samp_cntr + 0.5*v_tm(kk);
+    c_tm(kk) = c_samp_cntr + 0.5*u_tm(kk);
 end
 
 % convert matrices to vectors of valid measurements only
