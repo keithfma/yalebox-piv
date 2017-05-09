4a7a267489c0de1195b95305ba9e1b9a4cacd80d
keithma <keithfma@gmail.com>
2017-05-09 08:35:36 -0400

    Completed standalone wrapper for piv_series.m

--- piv_series_standalone.m
+++ piv_series_standalone.m
@@ -1,4 +1,4 @@
-function [] = piv_series_standalone(param_file)
+function [] = piv_series_standalone(image_file, param_file, piv_file)
 % function [] = piv_series_standalone(param_file)
 %
 % Wrapper function to run piv_series() as a compiled standalone executable.
@@ -6,11 +6,34 @@ function [] = piv_series_standalone(param_file)
 % system.
 %
 % Arguments:
+%   image_file: String, netCDF file containing pre-processed image series, as
+%       produced by prep_series()
 %   param_file: String, .MAT file containing all piv_series() input arguments,
 %       typically produced using a customized version of
 %       piv_get_param_template.m
+%   piv_file: String, netCDF file to write PIV results, will *not* overwrite
 % % 
 
-% load parameters from file
+% initialize parallel environment
+% NOTE: use existing pool if possible, faster for interactive development
+if isempty(gcp('nocreate')) 
+    if getenv('ENVIRONMENT')
+        % SCC: avoid use of remote disk and use only requested number of cores
+        fprintf('%s: Init parallel environment for SCC\n', mfilename);
+        cluster = parcluster('local');
+        cluster.JobStorageLocation = getenv('TMPDIR');
+        nslots = str2double(getenv('NSLOTS'));
+        parpool(cluster, nslots);
+    else
+        % not SCC: use default settings
+        fprintf('%s: Init default parallel environment\n', mfilename);
+        parpool();
+    end
+end
+
+% run PIV analysis
 args = load(param_file);
-print(args);
+piv_series(piv_file, image_file, args.step_range, args.gap, args.samp_len, ...
+    args.samp_spc, args.intr_len, args.num_pass, args.valid_radius, ...
+    args.valid_max, args.valid_eps, args.spline_tension, args.min_frac_data, ...
+    args.min_frac_overlap, true);
