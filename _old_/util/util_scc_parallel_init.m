bef21945b188e2d7d672a19fce769de9fac04cbe
Keith Ma <keithfma@gmail.com>
2019-02-21 02:09:05 -0500

    Deletes a bunch of old unused code

--- _old_/util/util_scc_parallel_init.m
+++
@@ -1,7 +0,0 @@
-% Script to initialize a parallel environment on the BU SCC cluster.
-
-cluster = parcluster('local');
-cluster.JobStorageLocation = getenv('TMPDIR');
-nslots = str2double(getenv('NSLOTS'));
-cluster.NumWorkers = nslots;
-parpool(cluster, nslots);
--- _old_/util/util_setup_env.m
+++
@@ -1,8 +0,0 @@
-function [] = util_setup_env()
-% Add dependencies for the Yalebox PIV project.
-
-top_dir = fileparts(which(mfilename));
-addpath(top_dir);
-
-dep_dir = fullfile(top_dir, 'depend');
-addpath(genpath(dep_dir));
