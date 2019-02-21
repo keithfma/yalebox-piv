56aac10953c1599751671a6259599fb6d2a8d381
Keith Ma <keithfma@gmail.com>
2019-02-20 19:36:10 -0500

    prep_series_from_file is now the standard prep interface

--- prep.m
+++ prep.m
@@ -1,24 +1,24 @@
-function [] = prep_series_from_file(result_file, param_file)
-% function [] = prep_series_from_file(result_file, param_file)
+function [] = prep(output_mat, param_json)
+% function [] = prep(output_mat, param_json)
 % 
-% Create PIV input file for a given image series. Reads in the images,
-% rectifies and crops, masks, corrects illumination, and saves the results
-% and metadata in a netCDF file.
+% Pre-process image series for PIV analysis. Reads in the images, rectifies
+% and crops, masks, corrects illumination, and saves the results and
+% metadata in a MAT file.
 %
 % Simple wrapper around prep_series() that reads the (many) input
 % parameters from the standard JSON file rather than as arguments.
 %
 % Arguments:
-%   result_file = String, filename of the MAT input file to be created. 
-%   param_file: JSON file containing all input parameters
+%
+%   output_mat = String, filename of the MAT input file to be created. 
+% 
+%   param_json: JSON file containing all input parameters
 % %
 
 update_path('jsonlab', 'prep');
-
-pp = loadjson(param_file, 'SimplifyCell', 1);
-
+pp = loadjson(param_json, 'SimplifyCell', 1);
 prep_series(...
-    result_file, ...
+    output_mat, ...
     pp.image_dir.value, ...
     pp.exp_files.value, ...
     pp.view.value, ...
