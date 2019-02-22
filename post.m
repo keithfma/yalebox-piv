9107ac1e706a6a9aaf586bae2f70a755b4b3cc6e
Keith Ma <keithfma@gmail.com>
2019-02-21 19:36:58 -0500

    Top-level functions accept params as path or struct

--- post.m
+++ post.m
@@ -1,5 +1,5 @@
-function [] = post(param_json, piv_mat, post_mat)
-% function [] = post(param_json, piv_mat, post_mat)
+function [] = post(param, piv_mat, post_mat)
+% function [] = post(param, piv_mat, post_mat)
 % 
 % Post-processing for a PIV analysis output. Simple wrapper around
 % post_series() that reads the input parameters from the standard JSON file
@@ -7,8 +7,9 @@ function [] = post(param_json, piv_mat, post_mat)
 %
 % Arguments:
 % 
-%   param_json: JSON file containing all input parameters
-% 
+%   param: Either (a) string, path to JSON file containing all input
+%       parameters, or (b) struct containing same
+%
 %   piv_mat = String, filename of the MAT file containing pre-processed
 %       experiment images
 %
@@ -16,7 +17,16 @@ function [] = post(param_json, piv_mat, post_mat)
 % %
 
 update_path('jsonlab');
-param = loadjson(param_json, 'SimplifyCell', 1);
+
+switch class(param)
+    case 'struct'
+        param = param;  % do nothing
+    case 'char'
+        param = loadjson(param, 'SimplifyCell', 1);
+    otherwise
+        error('Unexpected type for input argument "param"');
+end
+
 post_series(...
     piv_mat, ...
     post_mat, ...
