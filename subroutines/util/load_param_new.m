ba88d32a540b1989612532134e2bfd048e5eb319
Keith Ma <keithfma@gmail.com>
2019-02-23 13:03:29 -0500

    Moves pack/unpack polygon utilities to util

--- subroutines/util/load_param_new.m
+++
@@ -1,7 +0,0 @@
-function param = load_param_new(param_file)
-% function param = load_param_new(param_file)
-% 
-% Load parameters from JSON file
-% %
-
-param = loadjson(param_file, 'SimplifyCell', 1);
