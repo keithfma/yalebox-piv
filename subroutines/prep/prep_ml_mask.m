97b660d91c99ceecdcf99365b3032f6afb1d1fd8
Keith Ma <keithfma@gmail.com>
2019-02-23 10:02:15 -0500

    Stashed before starting cleanup

--- subroutines/prep/prep_ml_mask.m
+++ subroutines/prep/prep_ml_mask.m
@@ -3,12 +3,6 @@ function mask = prep_ml_mask(rgb, sand_poly, other_poly)
 % TODO: add log messages
 % TODO: handle both training and prediction modes
 % TODO: post process the mask with morph filters (like before)
-% TODO: explore different values for the entropy filter size -- or just use
-%   several kernels as separate features
-% TODO: explore better models, rather than a shitty lil' decision tree
-% TODO: try unequal loss, I care more about NOT including bad pixels than
-%   losing good ones, morphology handles the fill for me
-% TODO: try downsampling the input points so I can use more features?
 
 % get training set indices
 % note: masks are invereted for this application
@@ -55,7 +49,9 @@ Y_train = [ones(size(sand_idx)); zeros(size(other_idx))];
 % random forest, version 2
 % note: minor improvement on the single tree, prob also more robust
 model = TreeBagger(10, X_train, Y_train, ...
-    'Cost', [0, 1; 1, 0]); %  , ...
+    'InBagFraction', 0.80, ...
+    'Cost', [0, 1; 1, 0], ...
+    'NumPrint', 1); %  , ...
     % 'Options', statset('UseParallel', true)); 
 
 % % decision tree with custom cost
@@ -76,6 +72,11 @@ model = TreeBagger(10, X_train, Y_train, ...
 
 % predict
 labels = predict(model, features);
+if iscell(labels)  % forests only
+    labels = double(cell2mat(labels));
+    labels(labels == double('1')) = true;
+    labels(labels == double('0')) = false;
+end
 labels = reshape(labels, size(rgb, 1), size(rgb, 2));
 
 % display
