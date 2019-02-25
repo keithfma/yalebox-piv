afbd92b9a8e87a3fd04ea144074bc29773a0fd4c
Keith Ma <keithfma@gmail.com>
2019-02-25 01:33:34 -0500

    Updates prep_mask_labels to define multiple classes

--- subroutines/prep/prep_mask_labels.m
+++ subroutines/prep/prep_mask_labels.m
@@ -1,5 +1,5 @@
 function [labels, poly_out] = prep_mask_labels(rgb, poly_in, interactive)
-% function [labels, polygons] = prep_mask_labels(rgb, polygons, interactive)
+% function [labels, polygons] = prep_mask_labels(rgb, poly_in, interactive)
 % 
 % Interactive tool for defining pixel classes using ROI polygons. Select
 % the region(s) that represent a single class (e.g. sand or other)
@@ -36,6 +36,7 @@ else
     poly_out = poly_in;
 end
 
+% TODO: do multiclass labeling
 mask = false(size(rgb, 1), size(rgb, 2));
 for ii = 1:length(poly_out)
     x_pts = poly_out{ii}(:, 1);
@@ -63,9 +64,12 @@ function outpoly = define_polygons(rgb, inpoly)
 margin = 0.025; % norm
 buffer = 0.01; % norm
 font = 14; % pt
-instruct = {'+ Create a polygon: click "Add" button and then click to define vertices', ...
-            '+ Edit a polygon: click andd vertices', ...
-            '+ Delete existing: right click and select "Delete Polygon"', ...
+% TODO: update instrictions
+instruct = {'Create polygons for each training class - the first class is sand', ...
+            '+ To create a polygon: click "Add" button and then click to define vertices', ...
+            '+ To change classes: click "Prev" or "Next" buttons', ...
+            '+ To edit a polygon: click and vertices', ...
+            '+ To delete a polygon: right click and select "Delete Polygon"', ...
             '+ Finish: click "Done"'};
 
 img_left = margin;
@@ -74,16 +78,28 @@ img_width = 1 - 2*margin;
 img_height = 1 - (img_bot + 2*margin);
 img_pos = [img_left, img_bot, img_width, img_height];
 
-add_left = margin;
-add_bot = margin;
-add_width = 0.1;
-add_height = 0.05;
+prev_left = margin;
+prev_bot = margin;
+prev_width = 0.1;
+prev_height = 0.05;
+prev_pos = [prev_left, prev_bot, prev_width, prev_height];
+
+add_left = prev_left + prev_width + buffer;
+add_bot = prev_bot;
+add_width = prev_width;
+add_height = prev_height;
 add_pos = [add_left, add_bot, add_width, add_height];
 
-done_left = add_left + add_width + buffer;
-done_bot = add_bot;
-done_width = add_width;
-done_height = add_height;
+next_left = add_left + add_width + buffer;
+next_bot = add_bot;
+next_width = add_width;
+next_height = add_height;
+next_pos = [next_left, next_bot, next_width, next_height];
+
+done_left = next_left + next_width + buffer;
+done_bot = next_bot;
+done_width = next_width;
+done_height = next_height;
 done_pos = [done_left, done_bot, done_width, done_height];
 
 help_left = done_left + done_width + buffer;
@@ -97,9 +113,17 @@ hf = figure('Units', 'Normalized', 'Outerposition', [0 0 1 1]);
 
 hax = axes('Units', 'Normalized', 'Position', img_pos);
 
-uicontrol('Style', 'pushbutton', 'Units', 'normalized', ...
-    'Position', add_pos, 'String', 'Add', 'FontSize', font, ...
+uicontrol('Style', 'pushbutton', 'Units', 'normalized', 'Tag', 'PrevBtn', ...
+    'Position', prev_pos, 'String', 'Prev Class', 'FontSize', font, ...
+    'Callback', @do_prev);
+
+uicontrol('Style', 'pushbutton', 'Units', 'normalized', 'Tag', 'AddBtn', ...
+    'Position', add_pos, 'String', 'Add Poly', 'FontSize', font, ...
     'Callback', @do_add);
+
+uicontrol('Style', 'pushbutton', 'Units', 'normalized', 'Tag', 'NextBtn', ...
+    'Position', next_pos, 'String', 'Next Class', 'FontSize', font, ...
+    'Callback', @do_next);
 
 uicontrol('Style', 'pushbutton', 'Units', 'normalized', ...
     'Position', done_pos, 'String', 'Done', 'FontSize', font, ...
@@ -109,28 +133,94 @@ uicontrol('Style', 'pushbutton', 'Units', 'normalized', ...
     'Position', help_pos, 'String', 'Help', 'FontSize', font, ...
     'Callback', @(~,~)msgbox(instruct));
 
+colors = lines;
+setappdata(hf, 'class_colors', colors);
+setappdata(hf, 'prev_class', 1);
+setappdata(hf, 'current_class', 1);
+setappdata(hf, 'next_class', 2);
+update_colors();
+
 % display image and input polygons 
 imshow(rgb);
 for ii = 1:length(inpoly)
-    images.roi.Polygon(gca, 'Tag', 'ROI', 'Position', inpoly{ii});
+    images.roi.Polygon(gca, ...
+        'Tag', 'Class', ...
+        'Position', inpoly{ii}, ...
+        'Color', colors(ii, :), ...
+        'UserData', ii);
 end
 hax.YDir = 'normal';
 
 % wait here until user presses "Done"
 uiwait(); 
 
-% retreive and reformat output polygons
+% % retreive and reformat output polygons
 objs = findobj(hf, 'Tag', 'ROI');
-outpoly = cell(size(objs));
+
+class_ids = [];
 for jj = 1:length(objs)
-    outpoly{jj} = objs(jj).Position;
+    class_ids(end+1) = objs(jj).UserData;  %#ok<AGROW>
+end
+    
+outpoly = cell(size(unique(class_ids)));
+for jj = 1:length(outpoly)
+    outpoly{jj} = {};
+end
+
+for kk = 1:length(objs)
+    class_id = objs(kk).UserData;
+    outpoly{class_id}{end+1} = objs(kk).Position;
 end
 
 close(hf)
 
 
-function [] = do_add(~, ~)
-% Callback for add mask layer button
+function [] = update_colors()
+% Utility for updating colors when current class changes
 % %
-drawpolygon('Tag', 'ROI');
+colors = getappdata(gcf, 'class_colors');
+
+prev_obj = findobj(gcf, 'Tag', 'PrevBtn');
+prev_class = getappdata(gcf, 'prev_class');
+prev_obj.ForegroundColor = colors(prev_class, :);
+
+add_obj = findobj(gcf, 'Tag', 'AddBtn');
+current_class = getappdata(gcf, 'current_class');
+add_obj.ForegroundColor = colors(current_class, :);
+
+next_obj = findobj(gcf, 'Tag', 'NextBtn');
+next_class = getappdata(gcf, 'next_class');
+next_obj.ForegroundColor = colors(next_class, :);
+
+
+function [] = update_class(delta)
+% Update class indices
+% %
+current_class = getappdata(gcf, 'current_class');
+current_class = max(current_class + delta, 1);
+prev_class = max(current_class - 1, 1);
+next_class = current_class + 1;
+setappdata(gcf, 'prev_class', prev_class);
+setappdata(gcf, 'current_class', current_class);
+setappdata(gcf, 'next_class', next_class);
+update_colors();
+
+
+function [] = do_prev(~, ~)
+% Callback for "Prev Class" button
+% %
+update_class(-1);
+
+function [] = do_add(~, ~)
+% Callback for "Add Poly" button
+% %
+colors = getappdata(gcf, 'class_colors');
+current_class = getappdata(gcf, 'current_class');
+drawpolygon('Tag', 'ROI', 'Color', colors(current_class, :), 'UserData', current_class);
 uiwait();
+
+
+function [] = do_next(~, ~)
+% Callback for "Next Class" button
+% %
+update_class(1)
