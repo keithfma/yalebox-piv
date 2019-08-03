cf47821aa5144acc719571844c67ccbc5a22ea01
Keith Ma <keithfma@gmail.com>
2019-08-03 14:51:27 -0400

    Write out PIV results at each step to prevent data loss on error

--- subroutines/piv/piv_series.m
+++ subroutines/piv/piv_series.m
@@ -51,6 +51,14 @@ validateattributes(gap, {'numeric'}, {'scalar', 'positive', 'integer'});
 [~, ~, output_file_ext] = fileparts(output_file);
 assert(strcmp('.mat', output_file_ext), 'Output file must be .mat');
 
+% fail if output exists already
+assert(exist(output_file, 'file') == 0, ...
+    'Output file exists, either make space or choose another filename');
+
+% create output file 
+% note: populated progressively to preserve results in the event of an error
+output = matfile(output_file, 'Writable', true);
+
 % read in all required input data from file
 % note: reading a single time step takes about the same amount of time as
 %   reading the whole dataset, so we do not read a step-at-a time
@@ -77,12 +85,9 @@ ini_idx = ini_idx(in_range);
 fin_idx = fin_idx(in_range);
 
 % compute step coordinate vector
-step_piv = step_img(ini_idx);
+step_piv = step_img(ini_idx);  % assign results to initial time
+output.step = step_piv;  % save step coordinate vector
 num_step = length(step_piv);
-
-% fail if output exists already
-assert(exist(output_file, 'file') == 0, ...
-    'Output file exists, either make space or choose another filename');
 
 % define metadata
 meta = struct();
@@ -156,6 +161,8 @@ meta.mask.notes = 'true where sample window contains majority-sand, false elsewh
 meta.mask.dimensions = {'x', 'y', 'step'};
 meta.quality.units = 'boolean';
 
+output.meta = meta; % save to output file
+
 % analyse all steps
 for ii = 1:num_step
     
@@ -177,31 +184,28 @@ for ii = 1:num_step
         valid_max, valid_eps, min_frac_data, min_frac_overlap); 
     
     if ii == 1
-        % copy spatial coordinate vectors, created during PIV
-        x_piv = reshape(piv_data.x(1, :), size(piv_data.x, 2), 1);
-        y_piv = reshape(piv_data.y(:, 1), size(piv_data.y, 1), 1);
+        % copy and save spatial coordinate vectors, created during PIV
+        output.x = reshape(piv_data.x(1, :), size(piv_data.x, 2), 1);
+        output.y = reshape(piv_data.y(:, 1), size(piv_data.y, 1), 1);
         % allocate other output variables
         % note: num_step is forced to be >= 2 b/c there is no way to have
         %   a singleton as the last dimension
-        dims = [length(y_piv), length(x_piv), min(num_step, 2)];
-        u_piv = zeros(dims, 'double');
-        v_piv = zeros(dims, 'double');
-        qual_piv = zeros(dims, 'uint8');
-        mask_piv = zeros(dims, 'logical');
+        dims = [length(output.y), length(output.x), min(num_step, 2)];
+        allocate(output, 'u', 'double', dims);
+        allocate(output, 'v', 'double', dims);
+        allocate(output, 'quality', 'uint8', dims);
+        allocate(output, 'mask', 'logical', dims);
     end
     
-    % copy results to output arrays
-    u_piv(:, :, ii) = piv_data.u;
-    v_piv(:, :, ii) = piv_data.v;
-    qual_piv(:, :, ii) = Quality.to_uint8(piv_data.quality);
-    mask_piv(:, :, ii) = piv_data.mask;
+    % save results for this step
+    output.u(:, :, ii) = piv_data.u;
+    output.v(:, :, ii) = piv_data.v;
+    output.quality(:, :, ii) = Quality.to_uint8(piv_data.quality);
+    output.mask(:, :, ii) = piv_data.mask;
     
     fprintf('%s: end step = %.1f\n', mfilename,  step_piv(ii));
 end
 fprintf('%s: end series\n', mfilename);
-
-write_output_data(output_file, x_piv, y_piv, step_piv, u_piv, v_piv, ...
-    qual_piv, mask_piv, meta);
 
 
 function [x, y, step, img, mask] = read_input_data(input_file)
@@ -217,13 +221,3 @@ y = double(input_data.y);
 step = double(input_data.step);
 img = input_data.img;
 mask = input_data.mask;
-
-
-function write_output_data(output_file, x, y, step, u, v, quality, mask, meta)
-% function write_output_data(output_file, x, y, step, u, v, quality, mask, meta)
-%
-% Write results out to file
-% % 
-fprintf('%s: write output data to %s\n', mfilename, output_file);
-save(output_file, 'x', 'y', 'step', 'u', 'v', 'quality', 'mask', 'meta');
-
