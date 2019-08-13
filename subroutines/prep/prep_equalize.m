0d6beb55f8385d832220de659e112e39adce6ed1
Keith Ma <keithfma@gmail.com>
2019-08-12 22:43:14 -0400

    Enable equalization during image prep, saves results

--- subroutines/prep/prep_equalize.m
+++ subroutines/prep/prep_equalize.m
@@ -1,5 +1,5 @@
-function eql = prep_intensity(img, mask, eql_len, show)
-% function eql = prep_intensity(img, mask, eql_len, show)
+function img_eql = prep_equalize(img, mask, equalize_len, show)
+% function img_eql = prep_equalize(img, mask, equalize_len, show)
 %
 % Convert masked color image to "equalized" grayscale image. 
 %
@@ -7,8 +7,7 @@ function eql = prep_intensity(img, mask, eql_len, show)
 % pixel intensity distribution. A simple adaptive (local window) method is
 % used, in which the value of each pixel is replaced with its percentile in
 % a local window. This is equivalent to the normal method of using a CDF as
-% a transform function, but more efficient, since it is not necessary to
-% compute the CDF to convert just one pixel.
+% a transform function.
 %
 % Arguments:
 %
@@ -16,7 +15,7 @@ function eql = prep_intensity(img, mask, eql_len, show)
 % 
 %   mask: 2D matrix, double, TRUE where there is sand, FALSE elsewhere
 %
-%   eql_len: Scalar, integer, odd. Side length (in pixels) for the local
+%   equalize_len: Scalar, integer, odd. Side length (in pixels) for the local
 %       neighborhood used to compute the transform for each pixel.
 %
 %   show: Optional, scalar, logical flag, set to True to plot the original and
@@ -25,7 +24,7 @@ function eql = prep_intensity(img, mask, eql_len, show)
 %
 % Returns:
 %
-%   eql: 2D matrix, size(mask), double, normalized intensity image with uniform
+%   img_eql: 2D matrix, size(mask), double, normalized intensity image with uniform
 %       distribution in the range [0, 1].
 %
 % %
@@ -37,7 +36,7 @@ if nargin < 4; show = false; end
 % check for sane inputs
 validateattributes(img, {'numeric'}, {'3d'}); % 3 or fewer dims
 validateattributes(mask, {'logical'}, {'2d', 'size', [size(img,1), size(img, 2)]});
-validateattributes(eql_len, {'numeric'}, {'integer', 'positive', 'odd'});
+validateattributes(equalize_len, {'numeric'}, {'integer', 'positive', 'odd'});
 validateattributes(show, {'numeric', 'logical'}, {'scalar'});
 
 % convert rgb to grayscale
@@ -51,8 +50,8 @@ img(~mask) = 0;
 
 % get constants, preallocate variables
 [nr, nc] = size(img);
-eql = zeros(nr, nc);
-nhalfwin = floor(eql_len/2);
+img_eql = zeros(nr, nc);
+nhalfwin = floor(equalize_len/2);
 
 % report basic stats on intial image
 fprintf('%s: input image: min = %.1e, mean = %.1e, max = %.1e. std = %.1e\n', ...
@@ -76,13 +75,9 @@ for i = 1:nr
         
         % compute transform using percentile of the center point among its neighbors 
         nbr = img_win(mask_win);
-        eql(i,j) = (sum(nbr<img(i,j)) + 0.5*sum(nbr==img(i,j)))/numel(nbr);        
+        img_eql(i,j) = (sum(nbr<img(i,j)) + 0.5*sum(nbr==img(i,j)))/numel(nbr);        
     end
 end
-
-% add a tiny offset so that no sand pixels are exactly zero
-tiny = 1e-5;
-eql(mask & eql==0) = eql(mask & eql==0)+tiny;
 
 % (optional) show original and equalized intensity image
 if show
@@ -90,13 +85,11 @@ if show
     % plot original grayscale and equalized image
     figure    
     subplot(2,1,1)
-    imagesc(img);
-    caxis([0,1]);
+    imshow(img);
     axis off
     title('original grayscale');    
     subplot(2,1,2)
-    imagesc(eql);
-    caxis([0,1]);
+    imshow(img_eql);
     axis off
     title('equalized grayscale');
 
@@ -114,12 +107,12 @@ if show
     title('original grayscale histogram');
     
     subplot(2,2,2)    
-    [cdf, val] = ecdf(eql(mask));
+    [cdf, val] = ecdf(img_eql(mask));
     plot(val, cdf, 'Marker', '.');
     title('equalized grayscale CDF');
     
     subplot(2,2,4)
-    hist(eql(mask), nbins);
+    hist(img_eql(mask), nbins);
     title('equalized grayscale histogram');
     
 end
@@ -127,6 +120,4 @@ end
 
 % report basic stats on final image
 fprintf('%s: output image: min = %.1e, mean = %.1e, max = %.1e. std = %.1e\n', ...
-    mfilename, min(eql(mask)), mean(eql(mask)), max(eql(mask)), std(eql(mask)));
-
-
+    mfilename, min(img_eql(mask)), mean(img_eql(mask)), max(img_eql(mask)), std(img_eql(mask)));
