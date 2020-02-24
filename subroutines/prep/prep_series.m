cf2ef30ae9e269f4f6261ef10efb81526917eede
Keith Ma <keithfma@users.noreply.github.com>
2020-02-24 00:54:57 -0500

    fix: prep out of memory (#22)
    * prep_fill fixed to fully populate in the vertical
    
    * Matfile experiment first test case
    
    * Adds all-in-memory-serial
    
    Find that we are wasting time on random number generation, so will need a faster fill method
    
    * Add newmatic to dependencies
    
    * Adds newmatic test case, we have a winner!
    
    * Clear out losing cases
    
    * Successful experiment with parallel work and serial writes
    
    * prep_series uses newmatic with parallel processing and serial write
    
    * Remove need for global matfile variables in prep_series
    
    * Remove matfile-performance experiment, it is completed
    
    * Bump version

--- subroutines/prep/prep_series.m
+++ subroutines/prep/prep_series.m
@@ -58,7 +58,7 @@ function [] = prep_series(result_file, image_path, image_names, image_view, ...
 fprintf('%s: preprocessing %d images\n\n', mfilename, numel(image_names));
 
 % load dependencies
-update_path('prep', 'util');
+update_path('prep', 'util', 'newmatic');
 
 % set defaults
 narginchk(22, 23);
@@ -66,11 +66,13 @@ if nargin < 23; notes = ''; end
 
 % check for sane arguments (pass-through arguments are checked in subroutines)
 validateattributes(result_file, {'char'}, {'vector'});
+assert(exist(result_file, 'file') == 0, ...
+    'Output file exists, either make space or choose another filename');
+[~, ~, result_file_ext] = fileparts(result_file);
+assert(strcmp('.mat', result_file_ext), 'Output file must be .mat');
 validateattributes(image_path, {'char'}, {'vector'});
 validateattributes(image_names, {'cell'}, {'vector'});
 assert(any(strcmp(image_view, {'side', 'top'})), 'Invalid value for "image_view"');
-[~, ~, result_file_ext] = fileparts(result_file);
-assert(strcmp('.mat', result_file_ext), 'Output file must be .mat');
 
 % check that all images exist and have the expected size and type
 img_info = imfinfo(fullfile(image_path, image_names{1}));
@@ -89,11 +91,6 @@ for i = 1:numel(image_names)
     end
 end
 
-% create file, fail if exists
-assert(exist(result_file, 'file') == 0, ...
-    'Output file exists, either make space or choose another filename');
-result = matfile(result_file, 'Writable', true);
-
 % get coordinate vectors, some size parameters, and manual mask array
 % note: created during prep steps, so prep a fake image)
 fprintf('%s: get cordinate vectors and manual mask array\n', mfilename);
@@ -105,7 +102,127 @@ nx = numel(xw);
 ny = numel(yw);
 num_image = numel(image_names);
 
-% define output metadata
+% note: we take two passes over the data so that we can sniff the data extent
+%   and trim the arrays to the minimum size needed to fit the data
+
+% create temp file
+% note: we use this to hold intermediate results (images and masks before trimming)
+temp_file = [tempname, '.mat'];
+temp_file_cleanup = onCleanup(@() delete(temp_file));
+temp_mat = newmatic(temp_file, ...
+    newmatic_variable('imgs',  'uint8',   [ny, nx, 3, num_image], [ny, nx, 3, 1]), ...
+    newmatic_variable('masks', 'logical', [ny, nx, num_image],    [ny, nx, 1]) ...
+);
+
+% keep track of the maximum extent of the mask
+min_row = inf;  % set initial values so they are sure to be overridden
+max_row = -inf;
+min_col = inf;
+max_col = -inf;
+
+% read, rectify, crop, and mask all images
+temp_queue = parallel.pool.DataQueue;
+
+function write_step_temp(data)
+    % DataQueue callback function for serializing write step in parallel loop
+    data.mat.imgs(:, :, :, data.idx) = data.img;
+    data.mat.masks(:, :, data.idx) = data.mask;
+end
+
+afterEach(temp_queue, @write_step_temp);
+
+parfor ii = 1:num_image
+    
+    this_file = fullfile(image_path, image_names{ii});
+    img = imread(this_file);
+     
+    fprintf('\n%s: read, rectify, crop, and mask image %s\n', mfilename, this_file);
+    
+    img = prep_rectify_and_crop(...
+        ctrl_xp, ctrl_yp, ctrl_xw, ctrl_yw, crop_xw, crop_yw, img);
+
+    mask_auto = prep_mask_auto(...
+        img, hue_lim, value_lim, entropy_lim, entropy_len, image_view);
+    mask = logical(mask_auto & mask_manual);
+
+    % note: parfor magically reduces these values across workers
+    [mask_rows, mask_cols] = ind2sub(size(mask), find(mask));
+    min_row = min(min_row, min(mask_rows));
+    max_row = max(max_row, max(mask_rows));
+    min_col = min(min_col, min(mask_cols));
+    max_col = max(max_col, max(mask_cols));    
+    
+    % write is handled in serial by DataQueue callback
+    send(temp_queue, ...
+        struct('mat', temp_mat, 'idx', ii, 'img', uint8(img), 'mask', mask));
+    
+end
+
+% get size of trimmed data arrays (limited to the known data extent)
+nx = max_col - min_col + 1;  % size of reduced data arrays
+ny = max_row - min_row + 1;
+xw = xw(min_col:max_col);
+yw = yw(min_row:max_row);
+
+% get size and coordinate vectors for padded/extended arrays
+[xw_ext, yw_ext, ~, ~] = prep_pad(xw, yw, ones(ny, nx), true(ny, nx), pad_num_rows, pad_num_cols);
+nx_ext = numel(xw_ext);
+ny_ext = numel(yw_ext);
+
+% create output file
+results_mat = newmatic(result_file, ...
+    newmatic_variable('img',      'uint8',   [ny, nx, 3, num_image],      [ny, nx, 3, 1]), ...
+    newmatic_variable('mask',     'logical', [ny, nx, num_image],         [ny, nx, 1]), ...
+    newmatic_variable('img_ext',  'single',  [ny_ext, nx_ext, num_image], [ny, nx, 1]), ...
+    newmatic_variable('mask_ext', 'logical', [ny_ext, nx_ext, num_image], [ny, nx, 1]) ...
+);
+
+% equalize, pad, and fill all images
+results_queue = parallel.pool.DataQueue;
+
+function write_step_results(data)
+    % DataQueue callback function for serializing write step in parallel loop
+    data.mat.img(:, :, :, data.idx)   = data.img;
+    data.mat.img_ext(:, :, data.idx)  = data.img_ext;
+    data.mat.mask(:, :, data.idx)     = data.mask;
+    data.mat.mask_ext(:, :, data.idx) = data.mask_ext;
+end
+
+afterEach(results_queue, @write_step_results);
+
+parfor ii = 1:num_image
+    
+    fprintf('\n%s: equalize, pad, and fill image %i\n', mfilename, ii);
+    
+    % note: data is trimmed while reading it back from the temp file
+    img  = temp_mat.imgs( min_row:max_row, min_col:max_col, :, ii);  %#ok!
+    mask = temp_mat.masks(min_row:max_row, min_col:max_col, ii);
+     
+    img_gray = prep_grayscale(img);   
+    img_gray(~mask) = NaN;  % NaN indicates no image data, mask==true indicates sand
+    
+    [~, ~, img_ext, mask_ext] = prep_pad(...
+        xw, yw, img_gray, mask, pad_num_rows, pad_num_cols);
+    
+    img_ext = prep_fill(...
+        img_ext, mask_ext, fill_skin_min, fill_skin_max, fill_bnd_smooth_window);
+    
+    img_ext = prep_equalize(img_ext, ~isnan(img_ext), equalize_len);
+    
+    send(results_queue, struct(...
+        'mat', results_mat, 'idx', ii, 'img', img, 'img_ext', img_ext, 'mask', mask, 'mask_ext', mask_ext));
+  
+end
+
+% write coordinates to results file
+results_mat.x = xw;
+results_mat.y = yw;
+results_mat.x_ext = xw_ext;
+results_mat.y_ext = yw_ext;
+results_mat.band = 'RGB';
+results_mat.step = 0:(num_image - 1);
+
+% write metadata to results file
 meta = struct();
 meta.notes = notes;
 meta.version = get_version();
@@ -184,110 +301,12 @@ meta.mask_auto.notes = '';
 meta.mask_auto.dimensions = {'y', 'x', 'step'};
 meta.mask_auto.units = 'boolean';
 
-result.meta = meta;
+meta.mask_auto.name = 'mask_ext';
+meta.mask_auto.long_name = 'extended sand mask';
+meta.mask_auto.notes = '';
+meta.mask_auto.dimensions = {'y_ext', 'x_ext', 'step'};
+meta.mask_auto.units = 'boolean';
 
-% allocate output arrays
-% note: store results in memory and write to file at once because
-%   incremental writes to matfile slow to a crawl, allocating the variables
-%   at the outset ensures that everything fits in memory
-imgs = zeros(ny, nx, 3, num_image, 'uint8');
-masks = false(ny, nx, num_image);
+results_mat.meta = meta;
 
-% keep track of the maximum extent of the mask
-min_row = inf;  % set initial values so they are sure to be overridden
-max_row = -inf;
-min_col = inf;
-max_col = -inf;
-
-% read, rectify, crop, and mask all images
-% note: will crop out empty space before pad, equalize and fill
-parfor ii = 1:num_image
-    
-    this_file = fullfile(image_path, image_names{ii});
-    img = imread(this_file);
-     
-    fprintf('\n%s: read, rectify, crop, and mask image %s\n', mfilename, this_file);
-    
-    img = prep_rectify_and_crop(...
-        ctrl_xp, ctrl_yp, ctrl_xw, ctrl_yw, crop_xw, crop_yw, img);
-
-    mask_auto = prep_mask_auto(...
-        img, hue_lim, value_lim, entropy_lim, entropy_len, image_view);
-    mask = logical(mask_auto & mask_manual);
-    
-    [mask_rows, mask_cols] = ind2sub(size(mask), find(mask));
-    min_row = min(min_row, min(mask_rows));
-    max_row = max(max_row, max(mask_rows));
-    min_col = min(min_col, min(mask_cols));
-    max_col = max(max_col, max(mask_cols));    
-    
-    imgs(:, :, :, ii) = uint8(img);
-    masks(:, :, ii) = mask;
-    
 end
-
-% reduce size of data arrays
-nx = max_col - min_col + 1;  % size of reduced data arrays
-ny = max_row - min_row + 1;
-xw = xw(min_col:max_col);
-yw = yw(min_row:max_row);
-
-imgs = imgs(min_row:max_row, min_col:max_col, :, :);
-masks = masks(min_row:max_row, min_col:max_col, :);
-
-% get coordinates and size parameters for extended arrays
-[xw_ext, yw_ext, ~, ~] = prep_pad(xw, yw, ones(ny, nx), true(ny, nx), pad_num_rows, pad_num_cols);
-nx_ext = numel(xw_ext);
-ny_ext = numel(yw_ext);
-
-% allocate extended arrays
-imgs_ext = zeros(ny_ext, nx_ext, num_image, 'single');
-masks_ext = false(ny_ext, nx_ext, num_image);
-
-% equalize, pad, and fill all images
-% note: will crop out empty space before pad, equalize and fill
-parfor ii = 1:num_image
-    
-    img = imgs(:, :, :, ii);
-    mask = masks(:, :, ii);
-     
-    fprintf('\n%s: equalize, pad, and fill image %i\n', mfilename, ii);
-    
-    img = prep_grayscale(img); 
-    
-    img(~mask) = NaN;  % NaN indicates no image data, mask==true indicates sand
-    
-    [~, ~, img_ext, mask_ext] = prep_pad(...
-        xw, yw, img, mask, pad_num_rows, pad_num_cols);
-    
-    [img_ext] = prep_fill(...
-        img_ext, mask_ext, fill_skin_min, fill_skin_max, fill_bnd_smooth_window);
-    
-    img_ext = prep_equalize(img_ext, ~isnan(img_ext), equalize_len);
-    
-    imgs_ext(:, :, ii) = single(img_ext);
-    masks_ext(:, :, ii) = mask_ext;
-    
-end
-
-% copy data from memory to results file
-% note: here we trim image data to maximum extent of mask i.e., exclude
-%   pixels that are always sand to reduce compute during PIV
-fprintf('\n%s: copy from memory to results file: %s\n', mfilename, result_file);
-fprintf('%s: images array size = %d x %d x 3 x %d\n', mfilename, ny, nx, num_image);
-fprintf('%s: masks array size = %d x %d x %d\n', mfilename, ny, nx, num_image);
-fprintf('%s: extended images array size = %d x %d x %d\n', mfilename, ny_ext, nx_ext, num_image);
-fprintf('%s: extended masks array size = %d x %d x %d\n', mfilename, ny_ext, nx_ext, num_image);
-
-result.x = xw;
-result.y = yw;
-result.x_ext = xw_ext;
-result.y_ext = yw_ext;
-result.band = 'RGB';
-result.step = 0:(num_image - 1);
-
-result.img = imgs;
-result.mask = masks;
-
-result.img_ext = imgs_ext;
-result.mask_ext = masks_ext;
