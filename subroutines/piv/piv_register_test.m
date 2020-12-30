3b76041d5be3105ba3bf0ed7baf77144f0754bb5
Keith Ma <keithfma@gmail.com>
2020-12-29 21:53:01 -0500

    Improved implementation of test data loading

--- subroutines/piv/piv_register_test.m
+++ subroutines/piv/piv_register_test.m
@@ -3,10 +3,6 @@
 
 % TODO: parameterize over valid methods (temporary)
 
-
-% TODO: make a fixture that loads a few samples of sand image and normalizes them, or, loads
-%   a pre-normalized sand image. I prefer the former approach because we stay current.
-
 % TODO: write a function that uses the sand image to generate sample and interrogation windows
 %   with suitable coordinate vectors for each, given, ...
 %   + size and position (in normalized coordinates) of the sample window
@@ -22,39 +18,29 @@
 %   + rgb_image: RGB image of sand cropped from a yalebox experiment (fault_ss_01_siden_150.jpg)
 
 
-
-
-
-
 classdef piv_register_test < matlab.unittest.TestCase
     
     properties (Constant)
-        test_data_mat = matfile(...
-            fullfile(fileparts(mfilename('fullpath')), 'test_data.mat'), ...
-            'Writable', false);
-        rgb_image = piv_register_test.test_data_mat.rgb_image;
-        gray_image = prep_grayscale(piv_register_test.rgb_image);
-        norm_image = prep_equalize(...
-            piv_register_test.gray_image, ...
-            true(size(piv_register_test.gray_image)), ...
-            31);
+        image = normalized_source_image()
     end
     
     methods (Test)
         
         function test_nothing(self)
             % Dummy test to see if we can get tests to run at all
+            % TODO: delete me!
             fprintf('Did nothing successfully\n'); 
         end
         
         function test_correct_displacement(self)
             % Check if we can recover known displacement with piv_register
             
-            reference_image = self.norm_image;
+            reference_image = self.nimage;
             reference_rows = 1:size(reference_image, 1);
             reference_cols = 1:size(reference_image, 2);
             
             % TODO NEXT: generate sample image and it's coordinates
+            keyboard
             
         end
         
@@ -63,6 +49,18 @@ classdef piv_register_test < matlab.unittest.TestCase
 end
 
 
-function img = load_and_normalize_source_image(f)
-    img = 5;
+function obj = test_data_mat()
+    % return read-only matfile object for the test data MAT-file
+    obj = matfile(...
+        fullfile(fileparts(mfilename('fullpath')), 'test_data.mat'), ...
+        'Writable', false);
+end
+
+
+function norm_image = normalized_source_image()
+    % return normalized image for registration tests
+    mat = test_data_mat();
+    rgb_image = mat.rgb_image;
+    gray_image = prep_grayscale(rgb_image);
+    norm_image = prep_equalize(gray_image, true(size(gray_image)), 31);
 end
