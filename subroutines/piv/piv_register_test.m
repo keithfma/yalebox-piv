b0c70255d121c1cd0ecb893177a2bf649b536e66
Keith Ma <keithfma@gmail.com>
2021-01-04 05:57:54 -0500

    piv_register test parameterized and passing

--- subroutines/piv/piv_register_test.m
+++ subroutines/piv/piv_register_test.m
@@ -3,12 +3,6 @@
 
 % TODO: parameterize over valid methods (temporary)
 
-% TODO: test:
-%   + integer offsets in x, y and both
-%   + non-integer offsets in x, y, and both
-%   + sample image located in all six "directions" in the interogation image
-
-
 % note: input data used by tests is stored in the file test_data.mat, which contains:
 %   + rgb_image: RGB image of sand cropped from a yalebox experiment (fault_ss_01_siden_150.jpg)
 
@@ -16,36 +10,54 @@
 classdef piv_register_test < matlab.unittest.TestCase
     
     properties (Constant)
+        % TODO: document what this section is for
+        % %
+        
         % read-only matfile object containing test data
         mat = matfile(...
             fullfile(fileparts(mfilename('fullpath')), 'test_data.mat'), ...
             'Writable', false);
-        % normalized reference image
-        reference_image = piv_register_test.create_reference_image();
-        % row and column coordinate vectors for reference image
-        reference_rows = 1:size(piv_register_test.reference_image, 1);
-        reference_cols = 1:size(piv_register_test.reference_image, 2);
+        % normalized source image used to create reference and sample images
+        source_image = piv_register_test.load_source_image();
+        % row and column coordinate vectors for source image
+        source_rows = 1:size(piv_register_test.source_image, 1);
+        source_cols = 1:size(piv_register_test.source_image, 2);
     end
     
     properties (TestParameter)
+        % TODO: document what this section is for
+        % %        
         
+        % TODO: describe
+        reference_trim = struct(...
+            'square', [0, 0], ...
+            'tall_odd', [0, 20], ...
+            'wide_even', [20, 0]);
+        % location of the sample window in the source image, as a [row, column] offset relative to
+        %   the center of the source image
+        sample_location = struct(...
+            'center', [0, 0], ...
+            'not_center', [-31.2, 33]);
         % dimensions of sample window
         sample_dims = struct(...
-            'small_odd_square', [31, 31], ...
-            'large_odd_square', [121, 121]); %, ...
-            % 'small_odd_rect', [31, 41], ...  % FAILS 
-            % 'small_even_square', [30, 30], ... % FAILS    
-            % 'large_odd_rect', [121, 131], ... % FAILS
-            % 'large_even_square', [120, 120]);  % FAILS
+            'square_even', [30, 30], ...
+            'square_odd', [31, 31], ...
+            'rect_mixed', [30, 41]);
+        % TODO: describe
         sample_translation = struct(...
             'none', [0, 0], ...
-            'small_int_pos_pos', [10, 12], ...  
-            'small_float_pos_pos', [10.1, 11.9]);
+            'small_int_pos_pos', [10, 12], ... 
+            'small_int_pos_neg', [10, -12], ...
+            'small_float_pos_pos', [10.1, 11.9], ...
+            'small_float_pos_neg', [10.1, -11.9], ...
+            'large_float_pos_neg', [70.3, -79.8]);
     end
     
     methods (Static)
+        % TODO: document what this section is for
+        % %
             
-        function img = create_reference_image()
+        function img = load_source_image()
             % return normalized image for registration tests
             mat = piv_register_test.mat;
             gray = prep_grayscale(mat.rgb_image);
@@ -55,6 +67,26 @@ classdef piv_register_test < matlab.unittest.TestCase
     end
     
     methods
+        % TODO: document what this section is for
+        % %        
+        
+        function [image, rows, cols] = create_reference_image(self, trim)
+            % return reference image and its coordinate vectors for specified test case
+            % %
+            
+            validateattributes(trim, {'numeric'}, {'vector', 'integer', 'numel', 2});
+            
+            self.assertTrue(size(self.source_image, 1) == size(self.source_image, 2), ...
+                'expect source image to be square');
+            
+            image = self.source_image(1:end-trim(1), 1:end-trim(2));
+            rows = self.source_rows(1:end-trim(1));
+            cols = self.source_cols(1:end-trim(2));
+            
+            self.assertTrue(all(size(image) == (size(self.source_image) - trim)), 'wrong image size')
+            self.assertTrue(length(rows) == size(self.source_image, 1) - trim(1), 'wrong row coord size');
+            self.assertTrue(length(cols) == size(self.source_image, 2) - trim(2), 'wrong col coord size');
+        end
         
         function [image, rows, cols] = create_sample_image(self, location, dimensions, translation)
             % return sample image and its coordinate vectors for specified test case
@@ -69,39 +101,42 @@ classdef piv_register_test < matlab.unittest.TestCase
             row_idx = (1:dimensions(1)) - dimensions(1)/2;
             col_idx = (1:dimensions(2)) - dimensions(2)/2;
             % ...adjust vectors so the new center is the reference center + location offset 
-            center_idx = (1 + size(self.reference_image))/2 + location;  
+            center_idx = (1 + size(self.source_image))/2 + location;  
             row_idx = row_idx + center_idx(1);
             col_idx = col_idx + center_idx(2);
             % ...check results satisfy assumptions
-            self.assertEqual(mean(row_idx), center_idx(1), 'AbsTol', 0.5, 'rows not centered');
-            self.assertEqual(mean(col_idx), center_idx(2), 'AbsTol', 0.5, 'columns not centered');
+            self.assertEqual(mean(row_idx), center_idx(1), 'AbsTol', 0.51, 'rows not centered');
+            self.assertEqual(mean(col_idx), center_idx(2), 'AbsTol', 0.51, 'columns not centered');
             self.assertLength(row_idx, dimensions(1), 'wrong row index length');
             self.assertLength(col_idx, dimensions(2), 'wrong column_index length');
             % note: we check that all indices are within the reference image after interpolation
             
-            % resample image and coordinates from reference
-            rows = interp1(1:size(self.reference_image, 1), self.reference_rows, row_idx, 'linear');
-            cols = interp1(1:size(self.reference_image, 2), self.reference_rows, row_idx, 'linear');
+            % resample image and coordinates from reference           
+            rows = interp1(1:size(self.source_image, 1), self.source_rows, row_idx, 'linear');
+            cols = interp1(1:size(self.source_image, 2), self.source_cols, col_idx, 'linear');
             image = interp2(...
-                1:size(self.reference_image, 1), ...
-                1:size(self.reference_image, 2), ...
-                self.reference_image, ...
-                row_idx', ...
-                col_idx, ...
+                self.source_cols, ...
+                self.source_rows, ...
+                self.source_image, ...
+                cols, ...
+                rows', ...
                 'cubic');
+            % ...check results satisfy assumptions
+            self.assertTrue(all(size(image) == dimensions), 'incorrect dimensions');
             
             % % DEBUG: plot reference image and resampled image on the same axes with markers for 
             % %   the corners of the resampled image
             % figure
-            % imagesc(self.reference_rows([1, end]), self.reference_cols([1, end]), self.reference_image);
+            %imagesc(self.source_cols([1, end]), self.source_rows([1, end]), self.source_image);
             % hold on
-            % imagesc(rows([1, end]), cols([1, end]), image);
+            % imagesc(cols([1, end]), rows([1, end]), image);
             % colormap('gray');
             % plot(...
+            %    [cols(1), cols(1), cols(end), cols(end)], ...
             %     [rows(1), rows(end), rows(end), rows(1)], ...
-            %     [cols(1), cols(1), cols(end), cols(end)], ...
-            %     'Color', 'r', 'Marker', '.', 'MarkerSize', 24, 'LineStyle', 'None');
-            % % /DEBUG
+            %    'Color', 'r', 'Marker', '.', 'MarkerSize', 24, 'LineStyle', 'None');
+            % keyboard
+            % /DEBUG
             
             % translate image coordinates such that it would have to be translated by 'translation'
             %   to correctly register against the reference image
@@ -114,33 +149,39 @@ classdef piv_register_test < matlab.unittest.TestCase
        
     
     methods (Test)
+        % TODO: document what this section is for
+        % %        
         
-        function test_correct_displacement(self, sample_dims, sample_translation)
+        function test_correct_displacement(self, reference_trim, sample_location, sample_dims, sample_translation)
             % Check if we can recover known displacement with piv_register
+            % TODO: define arguments when the dust has settled
+            % %
             
-            % DEBUG: constant versions of parameters
-            samp_location = [50, 50];
-            min_overlap = 0.8*prod(sample_dims);
+            % constants
+            ABSOLUTE_TOLERANCE = 0.1;  % pixels
+            MIN_OVERLAP = 0.8*prod(sample_dims);
             % /DEBUG
             
+            [reference_image, reference_rows, reference_cols] = self.create_reference_image(...
+                reference_trim);
+            
             [sample_image, sample_rows, sample_cols] = self.create_sample_image(...
-                samp_location, sample_dims, sample_translation);
+                sample_location, sample_dims, sample_translation);
             
             sample_origin = [sample_rows(1), sample_cols(1)];
             sample_mask = true(size(sample_image));
             
-            reference_origin = [self.reference_rows(1), self.reference_cols(1)];
-            reference_mask = true(size(self.reference_image));
-            
-            
+            reference_origin = [reference_rows(1), reference_cols(1)];
+            reference_mask = true(size(reference_image));
+                
             [delta_row, delta_col, quality] = piv_register(...
                 sample_image, sample_mask, sample_origin, ...
-                self.reference_image, reference_mask, reference_origin, ...
-                min_overlap); 
+                reference_image, reference_mask, reference_origin, ...
+                MIN_OVERLAP); 
             
             self.verifyEqual(quality, Quality.Valid);
-            self.verifyEqual(delta_row, sample_translation(1), 'AbsTol', 0.01);
-            self.verifyEqual(delta_col, sample_translation(2), 'AbsTol', 0.01);
+            self.verifyEqual(delta_row, sample_translation(1), 'AbsTol', ABSOLUTE_TOLERANCE);
+            self.verifyEqual(delta_col, sample_translation(2), 'AbsTol', ABSOLUTE_TOLERANCE);
             
         end
         
